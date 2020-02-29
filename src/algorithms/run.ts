f0eba8b1d818bb8ab7b88155f89ade62b0211f78
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-03-01 00:25:44 +0100

    feat: add mathjs, random and their examples

--- src/algorithms/run.ts
+++ src/algorithms/run.ts
@@ -1,3 +1,6 @@
+import * as math from 'mathjs'
+import random from 'random'
+
 import { CountryAgeDistribution } from '../assets/data/CountryAgeDistribution.types'
 import { SeverityTableRow } from '../components/Main/SeverityTable'
 import { AllParams } from './Param.types'
@@ -18,6 +21,37 @@ export default async function run(
   // console.log(JSON.stringify({ countryAgeDistribution }, null, 2))
 
   // TODO: run the actual algorithm
+
+  // DELETEME BEGIN: examples
+  //
+  // `mathjs` example:
+  //
+  // https://mathjs.org/docs/index.html
+  //
+  // const a = math.matrix([1, 4, 9, 16, 25])
+  // const b = math.matrix(math.ones([2, 3]))
+  // const d = [
+  //   [1, 2],
+  //   [3, 4],
+  // ]
+  // const e = math.matrix([
+  //   [5, 6],
+  //   [1, 1],
+  // ])
+  // const f = math.multiply(d, e)
+  // console.log(math.format(b.size()))
+  // console.log(math.format(f, 14))
+  //
+  //
+  // `random` example
+  //
+  // random.float(0, 1)
+  // random.int(0, 1)
+  // random.boolean()
+  // random.normal(0, 1)
+  // random.bernoulli(0.5)
+  //
+  // DELETEME END
 
   // TODO: return the actual result
   return { hello: 'world' }
