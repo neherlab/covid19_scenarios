614017eb47d0accb87c1d5584a95416b75708599
Nicholas Noll <nnoll523@gmail.com>
2020-03-03 09:31:24 -0800

    added in simple profile timer

--- src/algorithms/run.ts
+++ src/algorithms/run.ts
@@ -51,12 +51,13 @@ export default async function run(
       return dynamics.map(x => unpack(x));
   }
   
+  console.time('run');
   const sim: AlgorithmResult = {
       "deterministicTrajectory": simulate(initialState, poisson),
       "stochasticTrajectories": [],
       "params": modelParams
   };
-  console.log(sim["deterministicTrajectory"]);
+  console.timeEnd('run');
 
   // for (let i = 0; i < modelParams.numberStochasticRuns; i++) {
   //     sim.stochasticTrajectories.push(simulate(initialState, poisson));
