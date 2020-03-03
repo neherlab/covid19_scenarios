dfd7ee59ea1c5ba667f750c441b9abeee010d4cc
Nicholas Noll <nnoll523@gmail.com>
2020-03-03 07:48:44 -0800

    initial implementation of age based model

--- src/algorithms/agentmodel.js
+++ src/algorithms/agentmodel.js
@@ -1,3 +1,5 @@
+import infectionRate from './model.js'
+
 const index = {"0-9": 0,
              "10-19": 1,
              "20-29": 2,
@@ -40,14 +42,46 @@ export function populationParameters(params, severity) {
 // NOTE: The data structure of the population is layed out as such:
 // Pop = numberOfSubPopulations x numberOfAgeClasses matrix.
 // Subpoulations are (in order):
+// 0            1        2         3          4             5           6
 // Susceptible, Exposed, Infected, Recovered, Hospitalized, Discharged, Dead
 function newPopulation() {
     return new BigInt64Array(new ArrayBuffer(8 * index.length * 7));
 }
 
-export function evolveAgent(pop, param) {
+// TODO: Implement super spreaders
+// TODO: Implement imports (how to stratify by age?)
+export function evolveAgent(pop, P, sample) {
     const at = (i, j) => j + index.length*i;
 
-    const newTime = pop["time"] + param.timeDelta;
-    let newPop = newPopulation();
+    const state = pop["state"];
+    const newTime = pop["time"] + P.timeDelta;
+    const newPop  = newPopulation();
+    for (let i = 0; i < index.length; i++) {
+        newPop[i] = state[i];
+    }
+
+    // Compute total infectious class
+    let Itotal = 0; 
+    for (let i = 0; i < index.length; i++) {
+        Itotal += state[at(2, i)];
+    }
+
+    for (let i = 0; i < index.length; i++) {
+        const dE = sample(infectionRate(newTime,P)*P.timeDeltaDays*state[at(0,i)]*Itotal/P.populationServed);
+        const dI = sample(state[at(1, i)] * P.timeDeltaDays / P.incubationTime);
+        const dH = sample(state[at(2, i)] * P.timeDeltaDays * P.hospitalizedRate[i]);
+        const dR = sample(state[at(2, i)] * P.timeDeltaDays * P.recoveryRate[i]);
+        const dF = sample(state[at(4, i)] * P.timeDeltaDays * P.deathRate[i]);
+        const dD = sample(state[at(4, i)] * P.timeDeltaDays * P.dischargeRate[i]);
+
+        newPop[at(0, i)] -= dE;
+        newPop[at(1, i)] += dE - dI;
+        newPop[at(2, i)] += dI - dH - dR;
+        newPop[at(3, i)] += dR + dD;
+        newPop[at(4, i)] += dH - dF - dD;
+        newPop[at(5, i)] += dD;
+        newPop[at(6, i)] += dF;
+    }
+
+    return {"state": newPop, "time": newTime};
 }
