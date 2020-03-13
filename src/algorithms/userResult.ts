98392f9733abd49bc1c0c34b6d57a42cd606ee87
Richard Neher <richard.neher@unibas.ch>
2020-03-13 22:27:05 +0100

    covert tsv import to timeseries WIP

--- src/algorithms/userResult.ts
+++ src/algorithms/userResult.ts
@@ -4,7 +4,7 @@ export default function processUserResult(rawUserResult: string[]): UserResult {
   console.log({ rawUserResult })
 
   // These are the columns we got from currently exported files.
-  // They my not be present in the user results.
+  // They may not be present in the user results.
   const columns = [
     'time',
     'susceptible',
@@ -17,8 +17,29 @@ export default function processUserResult(rawUserResult: string[]): UserResult {
     'dead',
   ]
 
+
   // TODO: Parse trajectores, `throw` on errors
   const trajectory: SimulationTimePoint[] = []
+  const header = rawUserResult[0];
+  function parseRow(row: string[], idx: number[]) {
+    if (idx){
+      const newRow: SimulationTimePoint = {};
+      header.forEach((d,i) => {
+        try {
+          if (d === 'time'){
+            newRow[d] = new Date(row[i]);
+          } else if (columns.find(d) >= 0){
+            newRow[d] = parseFloat(row[i]);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      })
+      trajectory.push(newRow);
+    }
+  }
+  rawUserResult.forEach(parseRow);
+  console.log('parsed trajectory', trajectory)
 
   return { trajectory }
 }
