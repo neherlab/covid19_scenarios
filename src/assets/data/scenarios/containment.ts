4cce78e7b62e3415752b406baac413e7648e57c0
Richard Neher <richard.neher@unibas.ch>
2020-03-13 12:21:34 +0100

    defaults: make containment scenarios timeseries from the start [WIP]

--- src/assets/data/scenarios/containment.ts
+++ src/assets/data/scenarios/containment.ts
@@ -1,27 +1,35 @@
-import { ContainmentData } from '../../../algorithms/Param.types'
+import { ContainmentData, DateRange } from '../../../algorithms/Param.types'
+import { makeTimeSeries } from '../../../algorithms/TimeSeries'
+import simulationData from './simulation'
 
 export interface ContainmentScenario {
   name: string
   data: ContainmentData
 }
 
-const containmentScenarios: ContainmentScenario[] = [
+const reductions = [
   {
     name: 'No mitigation',
-    data: { reduction: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0] },
+    reduction: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
   },
   {
     name: 'Weak mitigation',
-    data: { reduction: [1.0, 0.9, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8] },
+    reduction: [1.0, 0.9, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
   },
   {
     name: 'Moderate mitigation',
-    data: { reduction: [1.0, 0.8, 0.7, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6] },
+    reduction: [1.0, 0.8, 0.7, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
   },
   {
     name: 'Strong mitigation',
-    data: { reduction: [1.0, 0.7, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.45, 0.45] },
+    reduction: [1.0, 0.7, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.45, 0.45],
   },
 ]
+
+const containmentScenarios: ContainmentScenario[] = reductions.map(d => {
+  console.log('red', d)
+  return {name: d.name, data: {reduction: makeTimeSeries(simulationData.simulationTimeRange, d.reduction)}}
+})
+console.log('ContainmentScenario', containmentScenarios)
 
 export default containmentScenarios
