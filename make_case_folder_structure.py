03cc71063bed5643f3a0254aef668497671cb02f
Nicholas Noll <nnoll523@gmail.com>
2020-03-17 09:22:46 +0100

    fix: used UN designated regions and subregions

--- make_case_folder_structure.py
+++ make_case_folder_structure.py
@@ -13,12 +13,15 @@ ROOT = "../data/case-counts"
 # Functions
 
 def get_regions(file):
-    countries = defaultdict(list)
+    countries = defaultdict(lambda: defaultdict(list))
     with open(file) as f:
         rdr = csv.reader(f)
         next(rdr)
         for row in rdr:
-            countries[row[3]].append(row[8])
+            countries[row[3]][row[5]].append(row[8])
+
+    for key, val in countries.items():
+        countries[key] = dict(val)
 
     return dict(countries)
 
@@ -26,14 +29,17 @@ def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def generate(continent, countries):
-    mkdir(f"{ROOT}/{continent}")
+def generate(root, continent, countries):
+    mkdir(f"{root}/{continent}")
     for c in countries:
-        mkdir(f"{ROOT}/{continent}/{c}")
+        mkdir(f"{root}/{continent}/{c}")
 
 # ------------------------------------------------------------------------
 # Main point of entry
 
 if __name__ == "__main__":
-    for continent, countries in get_regions(FILE).items():
-        generate(continent, countries)
+    for continent, subregions in get_regions(FILE).items():
+        root = f"{ROOT}/{continent}"
+        mkdir(root)
+        for subregion, countries in subregions.items():
+            generate(root, subregion, countries)
