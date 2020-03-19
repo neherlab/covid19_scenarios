3b0beb82ef6cb97528824ab57bc93b5705336a37
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2020-03-19 22:34:12 +0100

    Fixed yarn tests

--- config/jest/jest.tests.config.js
+++ config/jest/jest.tests.config.js
@@ -5,38 +5,41 @@ const { findModuleRoot } = require('../../lib/findModuleRoot')
 const { moduleRoot } = findModuleRoot()
 
 module.exports = {
-  rootDir: moduleRoot,
-  roots: ['<rootDir>/src'],
-  displayName: { name: 'test', color: 'cyan' },
-  testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-      diagnostics: {
-        pathRegex: /(\/__tests__\/.*|([./])(test|spec))\.[jt]sx?$/,
-        warnOnly: true,
-      },
+    rootDir: moduleRoot,
+    roots: ['<rootDir>/src'],
+    displayName: { name: 'test', color: 'cyan' },
+    testEnvironment: 'jest-environment-jsdom',
+    preset: 'ts-jest',
+    globals: {
+        'ts-jest': {
+            babelConfig: true,
+            diagnostics: {
+                pathRegex: /(\/__tests__\/.*|([./])(test|spec))\.[jt]sx?$/,
+                warnOnly: true,
+            },
+        },
     },
-  },
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.(spec|test).{js,jsx,ts,tsx}',
-  ],
-  moduleNameMapper: {
-    '\\.(eot|otf|webp|ttf|woff\\d?|svg|png|jpe?g|gif)$':
-      '<rootDir>/src/__mocks__/fileMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: [
-    '<rootDir>/config/jest/setupDotenv.js',
-    'jest-chain',
-    'jest-extended',
-    'jest-axe/extend-expect',
-    '@testing-library/jest-dom/extend-expect',
-  ],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
+    transform: {
+        '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/*.(spec|test).{js,jsx,ts,tsx}',
+    ],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '\\.(eot|otf|webp|ttf|woff\\d?|svg|png|jpe?g|gif)$': '<rootDir>/src/__mocks__/fileMock.js',
+        '\\.(css|scss)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: [
+        '<rootDir>/config/jest/setupDotenv.js',
+        'jest-chain',
+        'jest-extended',
+        'jest-axe/extend-expect',
+        '@testing-library/jest-dom/extend-expect',
+    ],
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+    ],
 }
--- src/algorithms/run.test.tsx
+++ src/algorithms/run.test.tsx
@@ -1,34 +1,32 @@
-import { AllParams, Month } from './Param.types'
+import {AllParamsFlat} from './Param.types'
 
-import countryAgeDistribution from './test-data/ageDistribution.small.example'
-import severity from './test-data/severity.small.example'
+import countryAgeDistribution from '../assets/data/country_age_distribution.json'
+import countryCaseCounts from '../assets/data/case_counts.json'
+import severityData from '../assets/data/severityData.json'
+import populationScenarios from '../assets/data/scenarios/populations'
+import epidemiologicalScenarios from '../assets/data/scenarios/epidemiological'
+import simulationData from '../assets/data/scenarios/simulation'
+import containmentScenarios from '../assets/data/scenarios/containment'
 
 import run from './run'
 
-const defaultParams: AllParams = {
-  populationServed: 100000,
-  ageDistribution: 'Germany',
-  suspectedCasesToday: 10,
-  importsPerDay: 2,
-  r0: 2.2,
-  serialInterval: 8,
-  seasonalForcing: 0.2,
-  peakMonth: Month.Jan,
+const defaultParams: AllParamsFlat = {
+  ...populationScenarios[0].data,
+  ...epidemiologicalScenarios[0].data,
+  ...simulationData
 }
 
 describe('run()', () => {
   it('should return hello world', async () => {
-    const result = await run(defaultParams, severity, countryAgeDistribution)
+    const result = await run(defaultParams, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
     expect(result).toBeObject()
-    expect(result?.hello).toBe('world')
   })
 
   it('should work for all r0', async () => {
     // Override r0 for this test
     const params = { ...defaultParams, r0: 0.85 }
 
-    const result = await run(params, severity, countryAgeDistribution)
+    const result = await run(params, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
     expect(result).toBeObject()
-    expect(result?.hello).toBe('world')
   })
 })
