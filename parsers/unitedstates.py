6fc717ad754111ca8f3a7509884c91d04319e1dd
Nicholas Noll <nnoll523@gmail.com>
2020-03-19 15:10:44 -0700

    added united states case counts

---
+++ parsers/unitedstates.py
@@ -0,0 +1,102 @@
+import os, sys
+import csv
+import json
+import requests
+import numpy as np
+
+from collections import defaultdict
+from .utils import write_tsv
+
+# ------------------------------------------------------------------------
+# Globals
+
+acronyms = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+    "AS": "American Samoa",
+    "DC": "District of Columbia",
+    "FM": "Federated States of Micronesia",
+    "GU": "Guam",
+    "MH": "Marshall Islands",
+    "MP": "Northern Mariana Islands",
+    "PW": "Palau",
+    "PR": "Puerto Rico",
+    "VI": "Virgin Islands",
+}
+
+URL  = "https://covidtracking.com/api/states/daily"
+LOC  = "case-counts/Americas/Northern America/United States of America"
+cols = ['time', 'cases', 'deaths', 'hospitalized', 'ICU', 'recovered']
+
+# ------------------------------------------------------------------------
+# Main point of entry
+
+def parse():
+    r  = requests.get(URL)
+    if not r.ok:
+        print(f"Failed to fetch {URL}", file=sys.stderr)
+        exit(1)
+        r.close()
+
+    db = json.loads(r.text)
+    r.close()
+
+    # Convert to ready made TSVs
+    regions = defaultdict(list)
+    for row in db:
+        date = str(row["date"])
+        date = f"{date[0:4]}-{date[4:6]}-{date[6:8]}"
+        elt  = [ date, row["positive"], row["death"], None, None, None ]
+        regions[acronyms[row["state"]]].append(elt)
+    regions = dict(regions)
+
+    for region, data in regions.items():
+        write_tsv(f"{LOC}/{region}.tsv", cols, data, "unitedstates")
