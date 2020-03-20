5a998f665844036160f8ab510e6ec7f635a75f17
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2020-03-20 07:33:14 +0100

    fix(tests): fix basic tests (yarn test) (#50)
    * Fixed yarn tests
    
    * Made a test that simulates multiple countries
    
    Co-authored-by: Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>

--- src/algorithms/run.test.tsx
+++ src/algorithms/run.test.tsx
@@ -1,34 +1,65 @@
-import { AllParams, Month } from './Param.types'
+import {AllParamsFlat} from './Param.types'
 
-import countryAgeDistribution from './test-data/ageDistribution.small.example'
-import severity from './test-data/severity.small.example'
+import countryAgeDistribution from '../assets/data/country_age_distribution.json'
+import countryCaseCounts from '../assets/data/case_counts.json'
+import severityData from '../assets/data/severityData.json'
+import populationScenarios from '../assets/data/scenarios/populations'
+import epidemiologicalScenarios from '../assets/data/scenarios/epidemiological'
+import simulationData from '../assets/data/scenarios/simulation'
+import containmentScenarios from '../assets/data/scenarios/containment'
 
 import run from './run'
 
-const defaultParams: AllParams = {
-  populationServed: 100000,
-  ageDistribution: 'Germany',
-  suspectedCasesToday: 10,
-  importsPerDay: 2,
-  r0: 2.2,
-  serialInterval: 8,
-  seasonalForcing: 0.2,
-  peakMonth: Month.Jan,
+const defaultParams: AllParamsFlat = {
+  ...populationScenarios[0].data,
+  ...epidemiologicalScenarios[1].data,
+  ...simulationData
 }
 
 describe('run()', () => {
   it('should return hello world', async () => {
-    const result = await run(defaultParams, severity, countryAgeDistribution)
+    const result = await run(defaultParams, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
     expect(result).toBeObject()
-    expect(result?.hello).toBe('world')
   })
 
   it('should work for all r0', async () => {
     // Override r0 for this test
     const params = { ...defaultParams, r0: 0.85 }
 
-    const result = await run(params, severity, countryAgeDistribution)
+    const result = await run(params, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
     expect(result).toBeObject()
-    expect(result?.hello).toBe('world')
+  })
+
+  it('should work for a lot of countries', async () => {
+
+    const countries = ["Switzerland", "Germany", "France", "Italy", "Spain", "Poland", "Romania", "Netherlands", "Belgium", "Czechia", "Greece", "Portugal", "Sweden", "Hungary", "Austria", "Bulgaria", "Denmark", "Finland", "Slovakia", "Ireland", "Croatia", "Lithuania", "Slovenia", "Latvia", "Estonia", "Cyprus", "Luxembourg", "Malta", "Canada", "United Kingdom", "United States"];
+    
+    const results : Record<string, any> = {};
+
+    for(let country of countries) {
+      try {
+        const countryAgeDistributionWithType = countryAgeDistribution as Record<string, any>;
+        const result = await run({
+          ...populationScenarios.filter(p => p.name === country)[0].data,
+          ...epidemiologicalScenarios[1].data,
+          ...simulationData
+        }, severityData, countryAgeDistributionWithType[country], containmentScenarios[3].data.reduction)
+        results[country] = result;
+      } catch(e) {
+        console.error(e);
+      }
+    }
+    let attributes = [...countries, 'deterministicTrajectory', 'time', 'total', 'infectious'];
+    console.log(JSON.stringify(results, function (name, val) {
+      if(!name) {
+        return val;
+      } else if(name.match(/^\d+$/)) {
+        return val;
+      } else if(attributes.includes(name)) {
+        return val;
+      } else {
+        return undefined
+      }
+    }, "  "))
   })
 })
