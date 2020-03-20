4effcf7b25338d9ed30a82923bf15f3e50e5f499
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-03-20 07:40:16 +0100

    fix: remove console.log, format

--- src/algorithms/run.test.tsx
+++ src/algorithms/run.test.tsx
@@ -1,4 +1,4 @@
-import {AllParamsFlat} from './Param.types'
+import { AllParamsFlat } from './Param.types'
 
 import countryAgeDistribution from '../assets/data/country_age_distribution.json'
 import countryCaseCounts from '../assets/data/case_counts.json'
@@ -13,12 +13,17 @@ import run from './run'
 const defaultParams: AllParamsFlat = {
   ...populationScenarios[0].data,
   ...epidemiologicalScenarios[1].data,
-  ...simulationData
+  ...simulationData,
 }
 
 describe('run()', () => {
   it('should return hello world', async () => {
-    const result = await run(defaultParams, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
+    const result = await run(
+      defaultParams,
+      severityData,
+      countryAgeDistribution.Germany,
+      containmentScenarios[0].data.reduction,
+    )
     expect(result).toBeObject()
   })
 
@@ -26,40 +31,70 @@ describe('run()', () => {
     // Override r0 for this test
     const params = { ...defaultParams, r0: 0.85 }
 
-    const result = await run(params, severityData, countryAgeDistribution.Germany, containmentScenarios[0].data.reduction)
+    const result = await run(
+      params,
+      severityData,
+      countryAgeDistribution.Germany,
+      containmentScenarios[0].data.reduction,
+    )
     expect(result).toBeObject()
   })
 
   it('should work for a lot of countries', async () => {
+    const countries = [
+      'Switzerland',
+      'Germany',
+      'France',
+      'Italy',
+      'Spain',
+      'Poland',
+      'Romania',
+      'Netherlands',
+      'Belgium',
+      'Czechia',
+      'Greece',
+      'Portugal',
+      'Sweden',
+      'Hungary',
+      'Austria',
+      'Bulgaria',
+      'Denmark',
+      'Finland',
+      'Slovakia',
+      'Ireland',
+      'Croatia',
+      'Lithuania',
+      'Slovenia',
+      'Latvia',
+      'Estonia',
+      'Cyprus',
+      'Luxembourg',
+      'Malta',
+      'Canada',
+      'United Kingdom',
+      'United States',
+    ]
 
-    const countries = ["Switzerland", "Germany", "France", "Italy", "Spain", "Poland", "Romania", "Netherlands", "Belgium", "Czechia", "Greece", "Portugal", "Sweden", "Hungary", "Austria", "Bulgaria", "Denmark", "Finland", "Slovakia", "Ireland", "Croatia", "Lithuania", "Slovenia", "Latvia", "Estonia", "Cyprus", "Luxembourg", "Malta", "Canada", "United Kingdom", "United States"];
-    
-    const results : Record<string, any> = {};
+    const results: Record<string, any> = {}
 
-    for(let country of countries) {
+    for (let country of countries) {
       try {
-        const countryAgeDistributionWithType = countryAgeDistribution as Record<string, any>;
-        const result = await run({
-          ...populationScenarios.filter(p => p.name === country)[0].data,
-          ...epidemiologicalScenarios[1].data,
-          ...simulationData
-        }, severityData, countryAgeDistributionWithType[country], containmentScenarios[3].data.reduction)
-        results[country] = result;
-      } catch(e) {
-        console.error(e);
+        const countryAgeDistributionWithType = countryAgeDistribution as Record<string, any>
+        const result = await run(
+          {
+            ...populationScenarios.filter(p => p.name === country)[0].data,
+            ...epidemiologicalScenarios[1].data,
+            ...simulationData,
+          },
+          severityData,
+          countryAgeDistributionWithType[country],
+          containmentScenarios[3].data.reduction,
+        )
+        results[country] = result
+      } catch (e) {
+        console.error(e)
       }
     }
-    let attributes = [...countries, 'deterministicTrajectory', 'time', 'total', 'infectious'];
-    console.log(JSON.stringify(results, function (name, val) {
-      if(!name) {
-        return val;
-      } else if(name.match(/^\d+$/)) {
-        return val;
-      } else if(attributes.includes(name)) {
-        return val;
-      } else {
-        return undefined
-      }
-    }, "  "))
+    let attributes = [...countries, 'deterministicTrajectory', 'time', 'total', 'infectious']
   })
 })
