4b668be94198f7b8ec8cfc5d0798e9f5dc358b5c
Christian Wienbruch <51226915+chriswien@users.noreply.github.com>
2020-03-21 22:48:51 +0100

    Add files via upload

---
+++ parsers/germany.py
@@ -0,0 +1,67 @@
+import sys
+import requests
+import csv
+import io
+
+from collections import defaultdict
+from .utils import write_tsv
+
+# ------------------------------------------------------------------------
+# Globals
+
+bundesland_codes = {
+   "BW": "Baden-Württemberg",
+   "BY": "Bayern",
+   "BE": "Berlin",
+   "BB": "Brandenburg",
+   "HB": "Bremen",
+   "HH": "Hamburg",
+   "HE": "Hessen",
+   "MV": "Mecklenburg-Vorpommern",
+   "NI": "Niedersachsen",
+   "NW": "Nordrhein-Westfalen",
+   "RP": "Rheinland-Pfalz",
+   "SL": "Saarland",
+   "SN": "Sachsen",
+   "ST": "Sachsen-Anhalt",
+   "SH": "Schleswig-Holstein",
+   "TH": "Thüringen",
+}
+
+URL  = "https://github.com/chriswien/COVID19_CaseNumberDE/COVID19_Cases_Bundeslaender_DE.csv"
+LOC  = "case-counts/Europe/Western Europe/Germany"
+cols = ['time', 'bundesland', 'cases', 'deceased']
+
+# ------------------------------------------------------------------------
+# Functions
+
+def to_int(x):
+    if x == "NA" or x == "":
+        return None
+    else:
+        return int(x)
+
+# ------------------------------------------------------------------------
+# Main point of entry
+
+def parse():
+    r  = requests.get(URL)
+    if not r.ok:
+        print(f"Failed to fetch {URL}", file=sys.stderr)
+        exit(1)
+        r.close()
+
+    regions = defaultdict(list)
+    fd  = io.StringIO(r.text)
+    rdr = csv.reader(fd)
+    hdr = next(rdr)
+
+    for row in rdr:
+        date   = row[0]
+        bundesland = bundesland_codes[row[1]]
+        regions[bundesland].append([date, to_int(row[2]), to_int(row[3]), None, None, None])
+
+    for region, data in regions.items():
+#        if region != "Liechtenstein":
+        write_tsv(f"{LOC}/{region}.tsv", cols, data, "germany")
+#        else:
