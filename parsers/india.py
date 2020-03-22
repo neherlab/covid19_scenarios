8fe4e60577741bb144a93311ea8864d03723e051
Nils Ole Tippenhauer <noleti@gmx.de>
2020-03-22 14:56:23 +0100

    fixing/replacing the slash in a state name (causes problems on *nix, adding json support)

--- parsers/india.py
+++ parsers/india.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from collections import defaultdict
 from datetime import datetime
-from .utils import write_tsv
+from .utils import write_tsv, list_to_dict, store_json
 
 # ------------------------------------------------------------------------
 # Globals
@@ -55,8 +55,19 @@ def parse():
 
     for state, data in states.items():
         try:
+            state = state.replace(' / ','_')
             write_tsv(f"{LOC}/{state}.tsv", cols, data, "india")
         except FileNotFoundError as e:
             print(f"dumping data for '{state}' failed.")
             raise e
 
+    # prepare dict for json
+    regions2 = {}
+    for region, data in states.items():
+        if not (region == "India"):
+            regions2["IND-"+region] = data
+        else:
+            regions2[region] = data
+
+    regions3 = list_to_dict(regions2, cols)
+    store_json(regions3)
