a808eeea2c012ebffc8a72f93e449e7a478dd962
Richard Neher <richard.neher@unibas.ch>
2020-03-23 15:56:01 +0100

    first stab at fitting initial time

--- fit_initial_cases.py
+++ fit_initial_cases.py
@@ -8,7 +8,7 @@ fixed_slope = np.log(2)/doubling_time
 march1st = datetime(2020,3,1).toordinal()
 under_reporting = 5
 delay = 18
-fatalit_rate = 0.02
+fatality_rate = 0.02
 
 def fit_cumulative_trajectory(t, y):
     good_ind = (y>3)&(y<500)
@@ -32,13 +32,21 @@ def fit_population(pop):
     data_array = np.array(data_array)
     fit_on_deaths = fit_cumulative_trajectory(data_array[:,0], data_array[:,2])
     if fit_on_deaths:
-        val_on_march1st = np.exp(delay*fit_on_deaths["slope"])*np.exp(march1st*fit_on_deaths["slope"] + fit_on_deaths["intercept"])/fatalit_rate
-        return val_on_march1st
+        val_on_march1st = np.exp(delay*fit_on_deaths["slope"])*np.exp(march1st*fit_on_deaths["slope"] + fit_on_deaths["intercept"])/fatality_rate
+        if val_on_march1st<100:
+            return (val_on_march1st, march1st)
+        else:
+            t100 = int((np.log(100*np.exp(-delay*fit_on_deaths["slope"])*fatality_rate)-fit_on_deaths["intercept"])/fit_on_deaths["slope"])
+            return (100, t100)
     else:
         fit_on_cases = fit_cumulative_trajectory(data_array[:,0], data_array[:,1])
         if fit_on_cases:
             val_on_march1st = under_reporting*np.exp(march1st*fit_on_cases["slope"] + fit_on_cases["intercept"])
-            return val_on_march1st
+            if val_on_march1st<100:
+                return (val_on_march1st, march1st)
+            else:
+                t100 = int((np.log(100/under_reporting)-fit_on_deaths["intercept"])/fit_on_deaths["slope"])
+                return (100, t100)
         else:
             return None
 
@@ -55,13 +63,15 @@ if __name__ == '__main__':
     updated_data = []
     initialCasesIndex = header.index('suspectedCaseMarch1st')
     for d in data:
-        if d[0] in case_counts and d[0][:3]!='ITA' and d[0]!='Italy':
-            initialCases = fit_population(case_counts[d[0]])
-            if initialCases:
-                d[initialCasesIndex] = str(int(initialCases))
-        updated_data.append(d)
+        if d[0] in case_counts: # and d[0][:3]!='ITA' and d[0]!='Italy':
+            fit = fit_population(case_counts[d[0]])
+            if fit:
+                initialCases, startDate = fit
+                updated_data.append([d[0], str(max(1,int(initialCases))), datetime.fromordinal(startDate).strftime('%Y-%m-%d'), '2020-08-31'])
+            else:
+                updated_data.append([d[0], d[initialCasesIndex], '2020-03-01', '2020-08-31'])
 
-    with open("populationData.tsv", 'w') as fh:
-        fh.write('\t'.join(header)+'\n')
+    with open("initialCondition.tsv", 'w') as fh:
+        fh.write('\t'.join(['name', 'initialCases', 'tMin', 'tMax'])+'\n')
         for d in updated_data:
             fh.write('\t'.join(d)+'\n')
