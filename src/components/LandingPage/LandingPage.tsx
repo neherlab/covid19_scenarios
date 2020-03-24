bb8daf58535b7428761370b5300b56686ef8681d
Tryggvi Gylfason <tryggvi@spotify.com>
2020-03-24 22:17:08 +0000

    Mobile responsive.
    Also gain more control over the svg wave

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -13,15 +13,11 @@ function LandingPage() {
             <p>Tool that models COVID-19 outbreak and hospital demand</p>
           </div>
           <button className="landing-page__simulate-button">Simulate</button>
-          <img className="landing-page__promo-image" src={promoImage} alt="Simulator showing results" />
-          <svg className="landing-page__wave" viewBox="0 0 60 10">
-            <pattern x="-6" id="waves" patternUnits="userSpaceOnUse" width="10" height="10">
-              <path d="M0 10 V5 Q2.5 2.5 5 5 T10 5 V10" fill="currentColor" />
-            </pattern>
-            <rect x="0" y="0" width="60" height="10" fill="url(#waves)" />
-          </svg>
         </div>
-        <div className="landing-page__content-section">Content</div>
+        <div className="landing-page__content-section">
+          <img className="landing-page__promo-image" src={promoImage} alt="Simulator showing results" />
+          Content
+        </div>
       </div>
     </>
   )
