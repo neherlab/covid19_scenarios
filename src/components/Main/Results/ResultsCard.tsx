9cbb38bf117aa81f3eff6e6303a558bb4632974a
Gavin Jefferies <gavin@runtime.io>
2020-03-23 23:41:57 -0700

    only render results if they shallowly change

--- src/components/Main/Results/ResultsCard.tsx
+++ src/components/Main/Results/ResultsCard.tsx
@@ -9,7 +9,7 @@ import { exportResult } from '../../../algorithms/utils/exportResult'
 import { AlgorithmResult, UserResult } from '../../../algorithms/types/Result.types'
 import processUserResult from '../../../algorithms/utils/userResult'
 
-import { EmpiricalData }from '../../../algorithms/types/Param.types'
+import { EmpiricalData } from '../../../algorithms/types/Param.types'
 
 import { CollapsibleCard } from '../../Form/CollapsibleCard'
 import FormSwitch from '../../Form/FormSwitch'
@@ -25,14 +25,14 @@ import { OutcomeRatesTable } from './OutcomeRatesTable'
 
 import { useTranslation } from 'react-i18next'
 
-export interface ResutsCardProps {
+interface ResultsCardProps {
   canRun: boolean
   severity: SeverityTableRow[] // TODO: pass severity throughout the algorithm and as a part of `AlgorithmResult` instead?
   result?: AlgorithmResult
   caseCounts?: EmpiricalData
 }
 
-function ResultsCard({ canRun, severity, result, caseCounts }: ResutsCardProps) {
+function ResultsCardFunction({ canRun, severity, result, caseCounts }: ResultsCardProps) {
   const { t } = useTranslation()
   const [logScale, setLogScale] = useState<boolean>(true)
 
@@ -71,7 +71,9 @@ function ResultsCard({ canRun, severity, result, caseCounts }: ResutsCardProps) 
       <Row noGutters>
         <Col>
           <p>
-            {t('This output of a mathematical model depends on model assumptions and parameter choices. We have done our best (in limited time) to check the model implementation is correct. Please carefully consider the parameters you choose and interpret the output with caution')}
+            {t(
+              'This output of a mathematical model depends on model assumptions and parameter choices. We have done our best (in limited time) to check the model implementation is correct. Please carefully consider the parameters you choose and interpret the output with caution',
+            )}
           </p>
         </Col>
       </Row>
@@ -79,13 +81,7 @@ function ResultsCard({ canRun, severity, result, caseCounts }: ResutsCardProps) 
         <Col>
           <div>
             <span>
-              <Button
-                className="run-button"
-                type="submit"
-                color="primary"
-                disabled={!canRun}
-                data-testid="RunResults"
-              >
+              <Button className="run-button" type="submit" color="primary" disabled={!canRun} data-testid="RunResults">
                 {t('Run')}
               </Button>
             </span>
@@ -137,4 +133,4 @@ function ResultsCard({ canRun, severity, result, caseCounts }: ResutsCardProps) 
   )
 }
 
-export { ResultsCard }
+export const ResultsCard = React.memo(ResultsCardFunction)
