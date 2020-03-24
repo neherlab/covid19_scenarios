9925671b30315a3a542f60f4ad300621c94166fb
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2020-03-24 19:19:47 +0100

    Fix parser by merging results when several sources where giving (by taking worst case scenario)

--- parsers/france.py
+++ parsers/france.py
@@ -32,7 +32,7 @@ def parse():
         exit(1)
         r.close()
 
-    regions = defaultdict(list)
+    regions = {}
     fd  = io.StringIO(r.text)
     rdr = csv.reader(fd)
     hdr = next(rdr)
@@ -40,9 +40,25 @@ def parse():
     for row in rdr:
         if row[1] == 'region':
             date   = row[0]
-            region = row[3]
-            regions[region].append([date, to_int(row[4]), to_int(row[5]), None, None, None])
+            region = row[3].replace(" ", "-").replace("Î", "I").replace("'", "").replace("’", "")
+            cases = to_int(row[4])
+            death = to_int(row[5])
 
+            if region not in regions:
+                regions[region] = {}
 
-    for region, data in regions.items():
-        write_tsv(f"{LOC}/{region}.tsv", cols, data, "france")
+            if date not in regions[region]:
+                regions[region][date] = [date, cases, death, None, None, None]
+                continue
+
+            # If data from another source is bigger, we take it
+            if cases is not None and (regions[region][date][1] is None or cases > regions[region][date][1]):
+                regions[region][date][1] = cases
+
+            if death is not None and (regions[region][date][2] is None or death > regions[region][date][2]):
+                regions[region][date][2] = death
+            
+
+    for region, dates in regions.items():
+        write_tsv(f"{LOC}/{region}.tsv", cols, [data for date,data in dates.items()], "france")
+
