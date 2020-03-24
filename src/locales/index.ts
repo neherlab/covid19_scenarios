5006c1561e5fa44464ef72b0ded41243a56a064f
Emmanuel Murillo <emmurillocr@gmail.com>
2020-03-23 20:56:24 -0600

    chore: spanish translation

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,5 +2,6 @@ import en from './en'
 import pt from './pt'
 import fr from './fr'
 import de from './de'
+import es from './es'
 
-export default { en, pt, fr, de }
+export default { en, pt, fr, de, es }
