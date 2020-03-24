b0cac2839708a84f41196631a516e9e68619b506
Joram van den Boezem <joram@vandenboezem.nl>
2020-03-24 23:13:05 +0100

    feat: add cases, deaths and hospitalized data for netherlands

--- parse_all.py
+++ parse_all.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     # initialize empty json
     with open('case-counts/case_counts.json', 'w') as fh:
         fh.write("{}")
-        
+
     srcs = list(json.load(open(SOURCES)).keys())
     for src in srcs:
-        print(f"Running {src}", file=sys.stderr)
-        country = importlib.import_module(f"parsers.{src}")
-        country.parse()
+        # Allow running this as `python parse_all.py netherlands` to filter sources (debug mode)
+        if len(sys.argv) == 1 or sys.argv[1] == src:
+            print(f"Running {src}", file=sys.stderr)
+            country = importlib.import_module(f"parsers.{src}")
+            country.parse()
--- parsers/netherlands.py
+++ parsers/netherlands.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 import json
+import csv
+import io
 
 from collections import defaultdict
 from datetime import datetime, timedelta
@@ -9,19 +11,56 @@ from .utils import store_data
 # ------------------------------------------------------------------------
 # Globals
 
+URL_CASES_CUM = "https://raw.githubusercontent.com/J535D165/CoronaWatchNL/master/data/rivm_corona_in_nl_daily.csv"
+URL_DEATHS_CUM = "https://raw.githubusercontent.com/J535D165/CoronaWatchNL/master/data/rivm_corona_in_nl_fatalities.csv"
+URL_HOSPITALIZED_CUM = "https://raw.githubusercontent.com/J535D165/CoronaWatchNL/master/data/rivm_corona_in_nl_hosp.csv"
 URL_ICU_CUM = "https://www.stichting-nice.nl/covid-19/public/intake-cumulative"
+
 LOC = "case-counts/Europe/Western Europe/Netherlands"
-cols = ['time', 'ICU']
+cols = ['time', 'cases', 'deaths', 'hospitalized', 'ICU', 'recovered']
 
 # ------------------------------------------------------------------------
 # Functions
 
 
-# ------------------------------------------------------------------------
-# Main point of entry
+def sorted_date(s):
+    return sorted(s, key=lambda d: datetime.strptime(d[cols.index('time')], "%Y-%m-%d"))
 
-def parse():
-    r  = requests.get(URL_ICU_CUM)
+
+# ------------------------------------------------------------------------
+# Sub parsers
+
+
+def parse_csv(regions_date, url, column):
+    r = requests.get(url)
+    if not r.ok:
+        print(f"Failed to fetch {url}", file=sys.stderr)
+        exit(1)
+        r.close()
+
+    fd = io.StringIO(r.text)
+    rdr = csv.reader(fd)
+    next(rdr)
+
+    for row in rdr:
+        date_string = row[0]
+        regions_date["Netherlands"][date_string][column] = row[1]
+
+
+def parse_cases(regions_date):
+    parse_csv(regions_date, URL_CASES_CUM, 'cases')
+
+
+def parse_deaths(regions_date):
+    parse_csv(regions_date, URL_DEATHS_CUM, 'deaths')
+
+
+def parse_hospitalized(regions_date):
+    parse_csv(regions_date, URL_HOSPITALIZED_CUM, 'hospitalized')
+
+
+def parse_icu(regions_date):
+    r = requests.get(URL_ICU_CUM)
     if not r.ok:
         print(f"Failed to fetch {URL}", file=sys.stderr)
         exit(1)
@@ -30,8 +69,6 @@ def parse():
     db = json.loads(r.text)
     r.close()
 
-    # Convert to ready made TSVs
-    regions = defaultdict(list)
     today = datetime.today()
     day_before_yesterday = today - timedelta(days=2)
     for row in db:
@@ -39,8 +76,35 @@ def parse():
         # Data from last 2 days may be incomplete, so we ignore it
         if date < day_before_yesterday:
             date_string = str(row["date"])
-            elt = [date_string, row["intakeCumulative"]]
-            regions["Netherlands"].append(elt)
+            regions_date["Netherlands"][date_string]['ICU'] = row["intakeCumulative"]
+
+
+# ------------------------------------------------------------------------
+# Main point of entry
+
+def parse():
+    regions_date = defaultdict(lambda: defaultdict(dict))
+
+    parse_cases(regions_date)
+    parse_deaths(regions_date)
+    parse_hospitalized(regions_date)
+    parse_icu(regions_date)
+
+    regions = defaultdict(list)
+    for region in regions_date:
+        for date in regions_date[region]:
+            entry = [
+                date,
+                regions_date[region][date].get('cases', None),
+                regions_date[region][date].get('deaths', None),
+                regions_date[region][date].get('hospitalized', None),
+                regions_date[region][date].get('ICU', None),
+                None
+            ]
+            regions[region].append(entry)
     regions = dict(regions)
+
+    for region in regions.keys():
+        regions[region] = sorted_date(regions[region])
 
     store_data(regions, {'default': LOC, 'Netherlands': LOC}, 'netherlands', 'NLD', cols)
