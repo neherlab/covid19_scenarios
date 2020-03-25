15292baa7a93d96379df0391bd4da87ef5860576
Nicholas Noll <nnoll523@gmail.com>
2020-03-25 13:40:53 -0700

    fix: added in mathjs dependency and removed gauss-jordan elimination function

--- src/algorithms/run.ts
+++ src/algorithms/run.ts
@@ -1,3 +1,5 @@
+import * as math from 'mathjs'
+
 import { OneCountryAgeDistribution } from '../assets/data/CountryAgeDistribution.types'
 
 import { SeverityTableRow } from '../components/Main/Scenario/SeverityTable'
@@ -16,85 +18,43 @@ const poisson = (x: number) => {
 export function interpolateTimeSeries(containment: TimeSeries): (t: Date) => number {
   const Ys = containment.map(d => d.y)
   const Ts = containment.map(d => Number(d.t)) 
+
   /* All needed linear algebra functions */
   type Vector = number[]
   type Matrix = number[][]
-  const swap_rows = function(A: Matrix, i: number, j: number) {
-      const tmp = A[i];
-      A[i] = A[j];
-      A[j] = tmp;
-  }
-
-  // NOTE: Assumes the matrix has already been augmented
-  //       This implies the last column is b (Ax = b)
-  const gj_eliminate = function(A : Matrix) : Vector {
-    const m = A.length;
-    const x = [];
-
-    // Columns
-    for (let k = 0; k < m; k++) {
-        let i_max = 0; 
-        let i_val = Number.NEGATIVE_INFINITY;
-        // Column pivot
-        for (let i=k; i<m; i++) {
-            const val = Math.abs(A[i][k])
-            if (val >i_val) {
-                i_max = i;
-                i_val = val;
-            }
-        }
-        swap_rows(A, k, i_max);
-
-        for (let i=k+1; i<m; i++) {
-            let cf = (A[i][k] / A[k][k]);
-            for (let j=k; j < m+1; j++) {
-                A[i][j] -= A[k][j] * cf;
-            }
-        }
-    }
-
-    // Rows
-    for (let i = m-1; i>=0; i--) {
-        const val = A[i][m] / A[i][i]
-        x[i] = val;
-        for (let j = i-1; j >= 0; j--) {
-            A[j][m] -= A[j][i] * val;
-            A[j][i] = 0;
-        }
-    }
-
-    console.log(A)
-
-    return x
-  }
 
   const getSmoothDerivatives = function(): Vector {
       // Solve for the derivatives that lead to "smoothest" interpolator
       let Mtx : Matrix = []
+      let Vec : Vector = []
       for (let i = 0; i < Ys.length; i++) {
           Mtx.push([]);
-          for (let j = 0; j < Ys.length+1; j++) {
+          for (let j = 0; j < Ys.length; j++) {
               Mtx[i].push(0);
           }
+          Vec.push(0)
       }
       const n = Mtx.length;
 
-      for (let i = 1; i<Ts.length-1; i++) {
+      for (let i = 1; i<n-1; i++) {
           Mtx[i][i-1] = 1 / (Ts[i] - Ts[i-1]);
           Mtx[i][i]   = 2 * (1/(Ts[i] - Ts[i-1]) + 1/(Ts[i+1]-Ts[i]));
           Mtx[i][i+1] = 1 / (Ts[i+1] - Ts[i]);
-          Mtx[i][n]   = 3*( (Ys[i]-Ys[i-1])/((Ts[i] - Ts[i-1])*(Ts[i] - Ts[i-1]))  +  (Ys[i+1]-Ys[i])/ ((Ts[i+1] - Ts[i])*(Ts[i+1] - Ts[i])) );
+          Vec[i]      = 3*( (Ys[i]-Ys[i-1])/((Ts[i] - Ts[i-1])*(Ts[i] - Ts[i-1]))  +  (Ys[i+1]-Ys[i])/ ((Ts[i+1] - Ts[i])*(Ts[i+1] - Ts[i])) );
       }
 
       Mtx[0][0] = 2/(Ts[1] - Ts[0]);
 	  Mtx[0][1] = 1/(Ts[1] - Ts[0]);
-	  Mtx[0][n] = 3 * (Ys[1] - Ys[0]) / ((Ts[1]-Ts[0])*(Ts[1]-Ts[0]));
+	  Vec[0]    = 3 * (Ys[1] - Ys[0]) / ((Ts[1]-Ts[0])*(Ts[1]-Ts[0]));
 		
 	  Mtx[n-1][n-2] = 1/(Ts[n-1] - Ts[n-2]);
       Mtx[n-1][n-1] = 2/(Ts[n-1] - Ts[n-2]);
-      Mtx[n-1][n]   = 3*(Ys[n-1] - Ys[n-2]) / ((Ts[n-1]-Ts[n-2])*(Ts[n-1]-Ts[n-2]));
+      Vec[n-1]      = 3*(Ys[n-1] - Ys[n-2]) / ((Ts[n-1]-Ts[n-2])*(Ts[n-1]-Ts[n-2]));
 
-      return gj_eliminate(Mtx);
+      const A = math.matrix(Mtx)
+      const x = math.multiply(math.inv(A), Vec);
+
+      return x.toArray();
   }
 
   const Yps = getSmoothDerivatives()
