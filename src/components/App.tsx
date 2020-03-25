db197f8740533ef36275ee438bf775aedb0b4e94
Tryggvi Gylfason <tryggvi@spotify.com>
2020-03-25 20:58:30 +0000

    Fix routing and start work on localstorage flag

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 
 import { withErrorBoundary } from 'react-error-boundary'
+import { Switch, Route, Redirect } from 'react-router'
 import ErrorPage from '../pages/ErrorPage'
 
-// import Layout from './Layout/Layout'
+import Layout from './Layout/Layout'
 import LandingPage from './LandingPage/LandingPage'
 
 import '../styles/global.scss'
 
 function App() {
+  const skipLandingPage = localStorage.getItem('skip-landing-page')
   return (
-    <>
-      <LandingPage />
-      {/* <Layout /> */}
-    </>
+    <Switch>
+      {!skipLandingPage && <Redirect exact from="/" to="/start" />}
+      <Route exact path="/start">
+        <LandingPage />
+      </Route>
+      <Route>
+        <Layout />
+      </Route>
+    </Switch>
   )
 }
 
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -12,11 +12,24 @@ function LandingPage() {
             <h1 className="landing-page__heading">COVID-19 Scenarios</h1>
             <p>Tool that models COVID-19 outbreak and hospital demand</p>
           </div>
-          <button className="landing-page__simulate-button">Simulate</button>
+          <button type="button" className="landing-page__simulate-button">
+            Simulate
+          </button>
         </div>
         <div className="landing-page__content-section">
           <img className="landing-page__promo-image" src={promoImage} alt="Simulator showing results" />
-          Content
+          <div className="landing-page__content-section-text">
+            <h1>Section</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis culpa fuga mollitia officiis
+              velit tempore eum nobis, eligendi debitis distinctio neque non labore placeat laudantium minima ut quos?
+              Molestiae!
+            </p>
+          </div>
+          <div>
+            <input id="skip-landing-page" type="checkbox" />
+            <label htmlFor="skip-landing-page">Skip straight to simulator next time</label>
+          </div>
         </div>
       </div>
     </>
