5caf3a83a4a0ee93ca7d26e800373107bb70cf9d
Rich Evans <rcb.evans@outlook.com>
2020-03-26 16:02:04 -0700

    Trim extra 0's from decimal places

--- src/helpers/numberFormat.ts
+++ src/helpers/numberFormat.ts
@@ -1,5 +1,5 @@
 import { numeral } from '../i18n'
 
 export function numberFormatter(humanize: boolean, round: boolean) {
-  return (value: number) => numeral(value).format(`0${!round ? '.000' : ''}${humanize ? 'a' : ''}`)
+  return (value: number) => numeral(value).format(`0${!round ? '.[000]' : ''}${humanize ? 'a' : ''}`)
 }
