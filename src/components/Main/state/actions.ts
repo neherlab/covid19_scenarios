6c8afb461b8ea2522504d2cb106e4f652769ebe2
Nicholas Noll <nnoll523@gmail.com>
2020-03-26 14:15:12 -0700

    simplified actions

--- src/components/Main/state/actions.ts
+++ src/components/Main/state/actions.ts
@@ -1,11 +1,6 @@
 import actionCreatorFactory from 'typescript-fsa'
 
-import {
-  ContainmentData,
-  EpidemiologicalData,
-  PopulationData,
-  SimulationData,
-} from '../../../algorithms/types/Param.types'
+import { ScenarioData } from '../../../algorithms/types/Param.types'
 
 const action = actionCreatorFactory('SCENARIO')
 
@@ -13,25 +8,10 @@ export interface SetScenarioParams {
   scenarioName: string
 }
 
-export const setOverallScenario = action<SetScenarioParams>('SET_OVERALL_SCENARIO')
-export const setPopulationScenario = action<SetScenarioParams>('SET_POPULATION_SCENARIO')
-export const setEpidemiologicalScenario = action<SetScenarioParams>('SET_EPIDEMIOLOGICAL_SCENARIO')
-export const setContainmentScenario = action<SetScenarioParams>('SET_CONTAINMENT_SCENARIO')
+export const setScenario = action<SetScenarioParams>('SET_SCENARIO')
 
-export interface SetPopulationDataParams {
-  data: PopulationData
-}
-export interface SetEpidemiologicalDataParams {
-  data: EpidemiologicalData
-}
-export interface SetContainmentDataParams {
-  data: ContainmentData
-}
-export interface SetSimulationData {
-  data: SimulationData
+export interface SetScenarioDataParams {
+  data: ScenarioData
 }
 
-export const setPopulationData = action<SetPopulationDataParams>('SET_POPULATION_DATA')
-export const setEpidemiologicalData = action<SetEpidemiologicalDataParams>('SET_EPIDEMIOLOGICAL_DATA')
-export const setContainmentData = action<SetContainmentDataParams>('SET_CONTAINMENT_DATA')
-export const setSimulationData = action<SetSimulationData>('SET_SIMULATION_DATA')
+export const setData = action<SetScenarioDataParams>('SET_SCENARIO_DATA')
