0f778928392e641732c13673f307e13ff3b298ac
Nicholas Noll <nnoll523@gmail.com>
2020-03-26 15:19:25 -0700

    fixed the types inferred from the scenario json

--- src/components/Main/state/data.ts
+++ src/components/Main/state/data.ts
@@ -2,13 +2,21 @@ export type Scenario = string
 
 import scenarios from '../../../assets/data/scenarios/scenarios.json'
 import { ScenarioData } from '../../../algorithms/types/Param.types'
+import { makeTimeSeries } from '../../../algorithms/utils/TimeSeries'
 
 export const scenarioNames = Object.keys(scenarios)
 
 export function getScenarioData(key: string): ScenarioData {
   if (!(key in scenarios)) {
-    throw new Error(`Error: scenario "${scenario}" not found in JSON`)
+    throw new Error(`Error: scenario "${key}" not found in JSON`)
   }
-  const data = scenarios[key]
+  // TODO: scenarios output should be typed
+  //       this is a hacky workaround for now
+  const data: ScenarioData = scenarios[key]
+
+  data.simulation.simulationTimeRange.tMin = new Date(data.simulation.simulationTimeRange.tMin)
+  data.simulation.simulationTimeRange.tMax = new Date(data.simulation.simulationTimeRange.tMax)
+  data.containment.reduction = makeTimeSeries(data.simulation.simulationTimeRange, data.containment.reduction)
+
   return data
 }
