01a42a0ad40b1ff9c5342c91dfba0de10e9ad2de
Nicholas Noll <nnoll523@gmail.com>
2020-03-26 16:22:14 -0700

    fix: attempt to write to read only fields in state data

--- src/components/Main/state/data.ts
+++ src/components/Main/state/data.ts
@@ -13,9 +13,14 @@ export function getScenarioData(key: string): ScenarioData {
   // TODO: scenarios output should be typed
   //       this is a hacky workaround for now
   const data: ScenarioData = scenarios[key]
+  console.log('KEY', key)
+  console.log('DATA', data)
 
-  data.simulation.simulationTimeRange.tMin = new Date(data.simulation.simulationTimeRange.tMin)
-  data.simulation.simulationTimeRange.tMax = new Date(data.simulation.simulationTimeRange.tMax)
+  data.simulation.simulationTimeRange = {
+    tMin: new Date(data.simulation.simulationTimeRange.tMin),
+    tMax: new Date(data.simulation.simulationTimeRange.tMax),
+  }
+
   data.containment.reduction = makeTimeSeries(data.simulation.simulationTimeRange, data.containment.reduction)
 
   return data
