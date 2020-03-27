36021e770a440e6197a6eb701bcbe90a4ecb67b7
Arkadiusz Góralski <a.goralski@tidio.net>
2020-03-27 20:35:46 +0100

    Added polish translation

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,5 +3,6 @@ import pt from './pt'
 import fr from './fr'
 import de from './de'
 import es from './es'
+import pl from './pl'
 
-export default { en, pt, fr, de, es }
+export default { en, pt, fr, de, es, pl }
