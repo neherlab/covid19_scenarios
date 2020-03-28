f923de882a3b4f11eb43ee97638005a1f941ecc3
maktarsis <tarsis.maksym@gmail.com>
2020-03-28 20:40:56 +0200

    refactor local storage stuff

--- src/state/ui/ui.reducer.ts
+++ src/state/ui/ui.reducer.ts
@@ -1,16 +1,15 @@
 import { Action } from 'redux'
 import { isType } from 'typescript-fsa'
 
+import LocalStorage, { LOCAL_STORAGE_KEYS } from '../../helpers/localStorage'
 import { setShouldSkipLandingPage } from './ui.actions'
-
-export const SKIP_LANDING_PAGE_KEY = 'skip-landing-page'
 
 export interface UiState {
   skipLandingPage: boolean
 }
 
 export const uiDefaultState: UiState = {
-  skipLandingPage: !!localStorage.getItem(SKIP_LANDING_PAGE_KEY),
+  skipLandingPage: Boolean(LocalStorage.get(LOCAL_STORAGE_KEYS.SKIP_LANDING_PAGE)),
 }
 
 export function uiReducer(state: UiState = uiDefaultState, action: Action) {
