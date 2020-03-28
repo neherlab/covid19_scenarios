a19c87f38a57b584c0fcca42697bb34ed5bb0753
Richard Neher <richard.neher@unibas.ch>
2020-03-28 17:43:52 +0100

    add routes and links

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ const routes: PageRouteDesc[] = [
   { path: '/about', page: 'About' },
   { path: '/team', page: 'Team' },
   { path: '/faq', page: 'Faq' },
+  { path: '/updates', page: 'Updates' },
 ]
 
 export default routes
