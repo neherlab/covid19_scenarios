1ee53fcdda989967b9a6c9175953efafac5d25d1
abrie <abrie@users.noreply.github.com>
2020-03-29 19:31:56 -0400

    Change 'require' to 'import' to fix eslint no-var-requires.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const webpack = require('@cypress/webpack-preprocessor')
+import webpack from '@cypress/webpack-preprocessor'
 
 /**
  * @type {Cypress.PluginConfig}
