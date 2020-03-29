4e661c376e3f8c6da04641d1f8161ca76dc37113
abrie <abrie@users.noreply.github.com>
2020-03-29 19:34:15 -0400

    Apply prettier style rules.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,14 +24,10 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import '@testing-library/cypress/add-commands';
+import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('closeDisclaimer', () => {
-  cy.findByText('COVID-19 Scenario Disclaimer')
-    .should('exist')
-    .next()
-    .click()
+  cy.findByText('COVID-19 Scenario Disclaimer').should('exist').next().click()
 
-  cy.findByText('COVID-19 Scenario Disclaimer')
-    .should('not.exist')
-});
+  cy.findByText('COVID-19 Scenario Disclaimer').should('not.exist')
+})
