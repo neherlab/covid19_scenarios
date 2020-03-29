2277fb7db97660aed113597dfc0e17713d7415c7
Gavin Jefferies <gavin@runtime.io>
2020-03-28 17:02:48 -0700

    save a scenario

--- src/components/MultipleScenarios/MultipleScenarios.tsx
+++ src/components/MultipleScenarios/MultipleScenarios.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import createPersistedState from 'use-persisted-state'
+import { v4 as uuidv4 } from 'uuid'
 
 import Main from '../Main/Main'
 
-function onScenarioSave(name: string, serializeScenario: string) {
-  console.log(name, serializeScenario)
-}
+const useSavedScenariosState = createPersistedState('savedScenarios')
+const useUserState = createPersistedState('user')
 
 export default function MultipleScenarios() {
+  const [user] = useUserState({ version: 1, id: uuidv4() })
+  const [savedScenarios, setSavedScenarios] = useSavedScenariosState({ version: 1, scenarios: [] })
+
+  function onScenarioSave(name: string, serializedScenario: string) {
+    setSavedScenarios({
+      version: 1,
+      scenarios: [...savedScenarios.scenarios, { id: uuidv4(), userid: user.id, name, serializedScenario } as never],
+    })
+  }
+
   return <Main onScenarioSave={onScenarioSave} />
 }
