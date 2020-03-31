f53c5f68162b66a5e3a253666ade454afdd4651a
Ariel <ariel.tonglet@gmail.com>
2020-03-30 23:22:26 -0300

    Adjust style and content for help tooltip
    Adds library to properly filter the "label" variable and display only the relevant information, standardizing the heading

--- src/components/Form/FormHelpButton.tsx
+++ src/components/Form/FormHelpButton.tsx
@@ -2,12 +2,26 @@ import React from 'react'
 
 import { Button, Card, CardBody, CardHeader, UncontrolledPopover } from 'reactstrap'
 
+import { onlyText } from 'react-children-utilities'
+
 import { FaQuestion } from 'react-icons/fa'
 
 import './FormHelpButton.scss'
 
 function safeId(id: string) {
   return id.replace('.', '-')
+}
+
+function getText(label: any) {
+  if (typeof label === 'string') {
+    return label
+  } else if (label.type === 'h2' || label.type === 'h3') {
+    return onlyText(label)
+  } else if (label.props && label.props.children) {
+    return onlyText(label.props.children[0])
+  } else {
+    return ''
+  }
 }
 
 export interface FormHelpButtonProps {
@@ -32,9 +46,11 @@ export default function FormHelpButton({ identifier, label, help }: FormHelpButt
         <FaQuestion className="help-button-icon" />
       </Button>
       <UncontrolledPopover placement="right" target={safeId(identifier)} trigger="legacy" hideArrow>
-        <Card>
-          <CardHeader>{label}</CardHeader>
-          <CardBody>{help}</CardBody>
+        <Card className="card--help">
+          <CardBody>
+            {label && <h4>{getText(label)}</h4>}
+            <p>{help}</p>
+          </CardBody>
         </Card>
       </UncontrolledPopover>
     </>
