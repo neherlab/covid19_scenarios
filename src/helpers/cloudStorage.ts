a720b2245be23ee1ea21f2ce39f48d215e3c0dc1
Mika <mika87@gmail.com>
2020-04-01 08:42:45 +0300

    Config to a different place

--- src/helpers/cloudStorage.ts
+++ src/helpers/cloudStorage.ts
@@ -1,14 +1,5 @@
 import firebase from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB_T7TXfC2cjOS3VgqGBd-Yn-7R_j2UzIk",
-  authDomain: "covid19-scenarios.firebaseapp.com",
-  databaseURL: "https://covid19-scenarios.firebaseio.com",
-  projectId: "covid19-scenarios",
-  storageBucket: "covid19-scenarios.appspot.com",
-  messagingSenderId: "880897171256",
-  appId: "1:880897171256:web:5b5a83a9d3045083d42bcd"
-}
+import { firebaseConfig } from '../../firebaseconf'
 
 async function init() {
   await firebase.initializeApp(firebaseConfig)
