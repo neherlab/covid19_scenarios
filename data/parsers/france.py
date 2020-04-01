5e847aeae273df0be89ea574c97fbc3161df72ed
Christian-G-UT <62850722+Christian-G-UT@users.noreply.github.com>
2020-04-01 22:00:29 +0200

    Fix France parser according to PR#76 in archived data repo
    Due to a conflict the merge of PR#76 in the archived data repo was not correct. This is the correct fix.

--- data/parsers/france.py
+++ data/parsers/france.py
@@ -71,6 +71,6 @@ def parse():
 
     regions2 = {}
     for reg, d in regions.items():
-        regions2['-'.join(['FRA',reg])] = [d[day] for day in sorted(d.keys()) if d[day][1]]
+        regions2['-'.join(['FRA',reg])] = [d[day] for day in sorted(d.keys())]
 
     store_data(regions2, 'france',  cols)
