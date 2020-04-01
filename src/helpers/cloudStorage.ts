3e93a04025a8bfa4f944048bdd929e2ef0df81df
Mika <mika87@gmail.com>
2020-04-01 12:15:49 +0300

    createUser creates also database collection, created setUserData, getUserData functions

--- src/helpers/cloudStorage.ts
+++ src/helpers/cloudStorage.ts
@@ -1,12 +1,24 @@
 import firebase from 'firebase/app'
 import { firebaseConfig } from '../../firebaseconf'
 
-async function init() {
+export async function init() {
   await firebase.initializeApp(firebaseConfig)
 }
 
-function createUserWithEmail(email: string, password: string) {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+export async function createUserWithEmail(email: string, password: string) {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+    const uid = firebase.auth().currentUser?.uid
+
+    // TODO better error
+    if (!uid) throw new Error('Something went wrong')
+
+    // TODO add default values or get them from localStorage
+    setUserData(uid, {})
+  }
+  catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 function signInWithEmail(email: string, password: string) {
@@ -21,4 +33,19 @@ function setAuthObserver(fn: Function) {
   firebase.auth().onAuthStateChanged(user => {
     fn(user)
   })
+}
+
+// TODO data typing
+function setUserData(uid: string, data: any) {
+  firebase.firestore().collection('users').doc(uid).set(data, { merge: true })
+}
+
+async function getUserData(uid: string) {
+  const doc = await firebase.firestore().collection('users').doc(uid).get()
+  
+  if (doc.exists) {
+    return doc.data()
+  }
+
+  throw new Error('User data not found')
 }
