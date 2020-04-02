7dd57cd59864b29163617ab8ba6e92390e1b4ce1
Mika <mika87@gmail.com>
2020-04-02 21:48:06 +0300

    Cleanup LoginForm and SignupForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 
 import { useFormik } from 'formik'
-import { Card, CardBody, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Card, CardBody, Col, Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 import { setSignupVisible, setLoginVisible } from '../../state/ui/ui.actions'
 import { signInWithEmail } from '../../helpers/cloudStorage'
@@ -29,42 +29,41 @@ function LoginForm() {
   }
 
   return (
-    <div className="form-container" onClick={() => dispatch(setLoginVisible({ loginVisible: false }))}>
-      <Card>
-        <CardBody>
-          <Col>
-            <Form onSubmit={formik.handleSubmit}>
-              <FormGroup>
-                <Label>E-mail</Label>
-                <Input 
-                  id="login-form-email"
-                  name="email"
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                  type="text"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label>Password</Label>
-                <Input 
-                  id="login-form-password"
-                  name="password"
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                  type="text"
-                />
-              </FormGroup>
-              <Button type="submit">
-                Login
-              </Button>
-              <Button type="button" onClick={handleNewUserClick}>
-                New user?
-              </Button>
-            </Form>
-          </Col>
-        </CardBody>
-      </Card>
-    </div>
+    <Modal isOpen={true} toggle={() => dispatch(setLoginVisible({ loginVisible: false }))}>
+      <ModalHeader toggle={() => dispatch(setLoginVisible({ loginVisible: false }))}>
+        Sign up
+      </ModalHeader>
+      <ModalBody>
+        <Form onSubmit={formik.handleSubmit}>
+          <FormGroup>
+            <Label>E-mail</Label>
+            <Input 
+              id="login-form-email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              type="text"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input 
+              id="login-form-password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              type="text"
+            />
+          </FormGroup>
+          <Button type="submit">
+            Login
+          </Button>
+          <Button type="button" onClick={handleNewUserClick}>
+            New user?
+          </Button>
+        </Form>
+      </ModalBody>
+    </Modal>
   )
 }
 
