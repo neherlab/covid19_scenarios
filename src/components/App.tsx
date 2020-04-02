4b68667e87b0aef7ad1f6252e24a4906afdc272c
Mika <mika87@gmail.com>
2020-04-02 21:16:32 +0300

    Firestore/localStorage data fetching logic in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import { withErrorBoundary } from 'react-error-boundary'
 import { Switch, Route, Redirect } from 'react-router'
 import ErrorPage from '../pages/ErrorPage'
 
-import { setAuthObserver } from '../helpers/cloudStorage'
+import { setAuthObserver, getUserData } from '../helpers/cloudStorage'
 import { setCurrentUserUid } from '../state/user/user.actions'
 
 import Layout from './Layout/Layout'
@@ -14,6 +14,8 @@ import LandingPage from './LandingPage/LandingPage'
 import '../styles/global.scss'
 import { State } from '../state/reducer'
 
+import localStorage, { LOCAL_STORAGE_KEYS } from '../helpers/localStorage'
+
 function App() {
   const dispatch = useDispatch()
   const shouldGoToLandingPage = useSelector(({ ui }: State) => !ui.skipLandingPage)
@@ -21,9 +23,23 @@ function App() {
   const handleAuthStateChange = (user: any) => {
     if (user) {
       dispatch(setCurrentUserUid({ currentUserUid: user.uid }))
+      fetchAndSetLocalStorageValues(user.uid)
     }
     else {
       dispatch(setCurrentUserUid({ currentUserUid: null }))
+    }
+  }
+
+  const fetchAndSetLocalStorageValues = async (uid: string) => {
+    const data: object | undefined = await getUserData(uid)
+    if (!data) {
+      return
+    } // TODO write localStorage values to firestore
+
+    for (let key in Object.keys(data)) {
+      if(Object.keys(LOCAL_STORAGE_KEYS).includes(key)) {
+        localStorage.set(key, data[key])
+      }
     }
   }
 
