c942ba539d8a3348fec9c9a62d939fbb4077ca98
Richard Neher <richard.neher@unibas.ch>
2020-04-03 10:15:35 +0200

    adjust severity table text and card header

--- src/components/Main/Scenario/SeverityCard.tsx
+++ src/components/Main/Scenario/SeverityCard.tsx
@@ -20,27 +20,36 @@ function SeverityCard({ severity, setSeverity, scenarioState, scenarioDispatch }
       identifier="severity-card"
       title={
         <>
-          <h3 className="my-1 text-wrap">{t('Severity assumptions and age-specific isolation')}</h3>
+          <h3 className="my-1 text-wrap">{t('Age group specific parameters')}</h3>
           <p className="my-0">
-            {t('based on data from')} {t('China')}
+            {t('Demographics')} -- {t('Disease severity')} -- {t('Group specific isolation')}
           </p>
         </>
       }
       help={t('Assumptions on severity which are informed by epidemiological and clinical observations in China')}
       defaultCollapsed
     >
-      <p>
-        {t(
-          'This table summarizes the assumptions on severity which are informed by epidemiological and clinical observations in China. The first column reflects our assumption on what fraction of infections are reflected in the statistics from China, the following columns contain the assumption on what fraction of the previous category deteriorates to the next. These fields are editable and can be adjusted to different assumptions. The last column is the implied infection fatality for different age groups.',
-        )}
-      </p>
-
       <SeverityTable
         severity={severity}
         setSeverity={setSeverity}
         scenarioState={scenarioState}
         scenarioDispatch={scenarioDispatch}
       />
+      <p>
+        {t('Summary of demographics and age dependent parameters.')} &nbsp;
+        {t('The second column shows how many people fall into each age group.')} &nbsp;
+        {t(
+          'The following columns summarize COVID19 severity informed by epidemiological and clinical observations from China. ',
+        )}
+        &nbsp;
+        {t(
+          'The column "Confirmed" reflects our assumption on what fraction of infections are reflected in the statistics, the following columns contain the assumption on what fraction of the previous category deteriorates to the next. ',
+        )}
+        &nbsp;
+        {t('Most fields are editable and can be adjusted to different assumptions.')} &nbsp;
+        {t('The last column can be used to specify age-group specific isolation measures.')}
+      </p>
+
     </CollapsibleCard>
   )
 }
