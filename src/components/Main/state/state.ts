765bc7aea69560a2fb0d074d4578d90aaf9c00aa
Gavin Jefferies <gavin@runtime.io>
2020-04-02 21:07:26 -0700

    add ageDistribution to scenarioState

--- src/components/Main/state/state.ts
+++ src/components/Main/state/state.ts
@@ -1,13 +1,13 @@
 import i18next from 'i18next'
-
-import { scenarioNames, getScenarioData } from './data'
-
+import { scenarioNames, getScenarioData, getAgeDistribution } from './data'
 import { ScenarioData } from '../../../algorithms/types/Param.types'
+import { OneCountryAgeDistribution } from '../../../assets/data/CountryAgeDistribution.types'
 
 export interface State {
   scenarios: string[]
   current: string
   data: ScenarioData
+  ageDistribution: OneCountryAgeDistribution
 }
 
 // TODO: Add a default category to export
@@ -16,8 +16,11 @@ export const CUSTOM_SCENARIO_NAME = i18next.t('Custom')
 
 export const defaultScenarioName = DEFAULT_OVERALL_SCENARIO_NAME
 
+const defaultScenarioData = getScenarioData(defaultScenarioName)
+
 export const defaultScenarioState: State = {
   scenarios: scenarioNames,
   current: defaultScenarioName,
-  data: getScenarioData(defaultScenarioName),
+  data: defaultScenarioData,
+  ageDistribution: getAgeDistribution(defaultScenarioData.population.country),
 }
