c7b7824a6fa4f5e1272c21d81419327136afa164
Mika <mika87@gmail.com>
2020-04-04 10:17:12 +0300

    Move firebaseconf to src, create firebaseconf.example

--- src/helpers/cloudStorage.ts
+++ src/helpers/cloudStorage.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-import { firebaseConfig } from '../../firebaseconf'
+import { firebaseConfig } from '../firebaseconf'
 
 export async function init() {
   await firebase.initializeApp(firebaseConfig)
