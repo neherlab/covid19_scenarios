69c706d40369cb417bb5580e30f4012a061ad240
Di Guan <diguan@microsoft.com>
2020-04-03 17:48:26 -0700

    fix Improve Landing page redirect logic #385

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { withErrorBoundary } from 'react-error-boundary'
-import { Switch, Route, Redirect } from 'react-router'
+import { Switch, Route } from 'react-router'
+
+import _ from 'lodash'
+
 import ErrorPage from '../pages/ErrorPage'
 
 import Layout from './Layout/Layout'
@@ -14,10 +17,13 @@ import { State } from '../state/reducer'
 function App() {
   const shouldGoToLandingPage = useSelector(({ ui }: State) => !ui.skipLandingPage)
   const initialQueryString = window.location.search
-
+  let history = useHistory()
   return (
     <Switch>
-      {shouldGoToLandingPage && <Redirect exact from="/" to="/start" />}
+      {shouldGoToLandingPage &&
+        history.location.pathname === '/' &&
+        _.isEmpty(history.location.search) &&
+        history.push('/start')}
       <Route exact path="/start">
         <LandingPage initialQueryString={initialQueryString} />
       </Route>
