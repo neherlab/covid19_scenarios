a9e278222fa43f39dc033cb4dc3b653d27dd113b
Danko Kozar <dakozar@microsoft.com>
2020-04-05 03:42:29 +0100

    feat: Using JSURL for data serialization in links

---
+++ src/components/Main/state/serialization/types/PersistedState.types.ts
@@ -0,0 +1,7 @@
+import { ScenarioData } from '../../../../../algorithms/types/Param.types'
+
+// part of the application state to be persisted in the URL
+export interface PersistedState {
+  current: string
+  data: ScenarioData
+}
