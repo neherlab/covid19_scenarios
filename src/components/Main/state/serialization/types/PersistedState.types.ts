9494f0f33b34c5e0dac83a1adb15f0f4e048a33d
Gavin Jefferies <gavin@runtime.io>
2020-04-05 13:30:09 -0700

    add age distribution to serialization

--- src/components/Main/state/serialization/types/PersistedState.types.ts
+++ src/components/Main/state/serialization/types/PersistedState.types.ts
@@ -1,7 +1,9 @@
 import { ScenarioData } from '../../../../../algorithms/types/Param.types'
+import { OneCountryAgeDistribution } from '../../../../../assets/data/CountryAgeDistribution.types'
 
 // part of the application state to be persisted in the URL
 export interface PersistedState {
   current: string
   data: ScenarioData
+  ageDistribution: OneCountryAgeDistribution
 }
