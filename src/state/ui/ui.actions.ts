f4707c21e3a669043ae7794786320dd1afe476d1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-04-07 15:55:46 +0200

    feat: remove Landing page

--- src/state/ui/ui.actions.ts
+++
@@ -1,9 +0,0 @@
-import actionCreatorFactory from 'typescript-fsa'
-
-const action = actionCreatorFactory('UI')
-
-export interface GenericUiParams {
-  shouldSkip: boolean
-}
-
-export const setShouldSkipLandingPage = action<GenericUiParams>('UI_SET_SHOULD_SKIP_LANDING_PAGE')
