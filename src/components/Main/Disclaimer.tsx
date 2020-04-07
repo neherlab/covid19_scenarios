dfac3ff0fb4d84892893e9d60289887bcb676f90
Bruno RZN <bruno@velours.io>
2020-04-08 01:37:37 +0200

    feat: add disclaimer for print / pdf

--- src/components/Main/Disclaimer.tsx
+++ src/components/Main/Disclaimer.tsx
@@ -7,6 +7,8 @@ import './Disclaimer.scss'
 
 import LocalStorage, { LOCAL_STORAGE_KEYS } from '../../helpers/localStorage'
 
+import logo from '../../assets/img/HIVEVO_logo.png'
+
 const disclaimerVersion = 0
 
 interface SuppressShowAgain {
@@ -19,6 +21,24 @@ function onDialogClosed(suppressShowAgain: boolean) {
     version: disclaimerVersion,
     suppressShowAgain,
   })
+}
+
+function DisclaimerContent() {
+  const { t } = useTranslation()
+  return (
+    <>
+      <p>
+        {t(
+          `This tool uses a mathematical model to simulate a variety of COVID-19 outcomes based on user-defined parameters. This output of the model depends on model assumptions and parameter choices.`,
+        )}
+      </p>
+      <p>
+        {t(
+          `It is not a medical predictor, and should be used for informational and research purposes only. Please carefully consider the parameters you choose. Interpret and use the simulated results responsibly. Authors are not liable for any direct or indirect consequences of this usage.`,
+        )}
+      </p>
+    </>
+  )
 }
 
 export default function DisclaimerProps() {
@@ -43,55 +63,69 @@ export default function DisclaimerProps() {
   const toggleChecked = () => setsuppressShowAgain(!suppressShowAgain)
 
   return (
-    <Modal
-      isOpen={showModal}
-      backdrop="static"
-      fade={false}
-      centered
-      autoFocus
-      toggle={toggle}
-      onClosed={() => onDialogClosed(suppressShowAgain)}
-    >
-      <ModalHeader>{t(`COVID-19 Scenarios`)}</ModalHeader>
-      <ModalBody>
-        <Row>
-          <Col>
-            <p>
-              {t(
-                `This tool uses a mathematical model to simulate a variety of COVID-19 outcomes based on user-defined parameters. This output of the model depends on model assumptions and parameter choices.`,
-              )}
-            </p>
-            <p>
-              {t(
-                `It is not a medical predictor, and should be used for informational and research purposes only. Please carefully consider the parameters you choose. Interpret and use the simulated results responsibly. Authors are not liable for any direct or indirect consequences of this usage.`,
-              )}
-            </p>
-          </Col>
-        </Row>
+    <>
+      <div className="d-none d-print-block p-break-after">
+        <h1 className="text-center">COVID-19 Scenarios</h1>
+        <p className="text-center">
+          COVID19-Scenarios allows to explore the dynamics of a COVID19 outbreak in a community and the anticipated
+          burden on the health care system.
+        </p>
 
-        <Row>
-          <Col>
-            <Form inline>
-              <FormGroup className="ml-auto">
-                <CustomInput
-                  id="dont-show-again"
-                  type="checkbox"
-                  onChange={toggleChecked}
-                  checked={suppressShowAgain}
-                />
-                <label htmlFor="dont-show-again" className="d-flex">
-                  {t(`Don't show again`)}
-                </label>
-              </FormGroup>
-            </Form>
-          </Col>
-        </Row>
-      </ModalBody>
-      <ModalFooter>
-        <Button className="mx-auto" color="danger" onClick={toggle}>
-          Accept
-        </Button>
-      </ModalFooter>
-    </Modal>
+        <div className="text-center p-logo">
+          <img alt="logo" src={logo} />
+        </div>
+        <p>
+          COVID19-Scenarios, as every other model, has parameters whose values are not known with certainty and that
+          might differ between places and with time. The values of some of these parameters have a big effect on the
+          results, especially those that determine how rapidly the outbreak spreads or how effective counter measures
+          are: some values will result in a small limited outbreak, others in a massive outbreak with many fatalities.
+          Furthermore, when extrapolating the outbreak into the future, the results will critically depend on
+          assumptions of <strong>future</strong> policy and the degree to which infection control measures are adhered
+          to. It is therefore important to interpret the model output with care and to assess the plausibility of the
+          parameter values and model assumptions.{' '}
+        </p>
+        <DisclaimerContent />
+      </div>
+      <Modal
+        isOpen={showModal}
+        backdrop="static"
+        fade={false}
+        centered
+        autoFocus
+        toggle={toggle}
+        onClosed={() => onDialogClosed(suppressShowAgain)}
+      >
+        <ModalHeader>{t(`COVID-19 Scenarios`)}</ModalHeader>
+        <ModalBody>
+          <Row>
+            <Col>
+              <DisclaimerContent />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form inline>
+                <FormGroup className="ml-auto">
+                  <CustomInput
+                    id="dont-show-again"
+                    type="checkbox"
+                    onChange={toggleChecked}
+                    checked={suppressShowAgain}
+                  />
+                  <label htmlFor="dont-show-again" className="d-flex">
+                    {t(`Don't show again`)}
+                  </label>
+                </FormGroup>
+              </Form>
+            </Col>
+          </Row>
+        </ModalBody>
+        <ModalFooter>
+          <Button className="mx-auto" color="danger" onClick={toggle}>
+            Accept
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
   )
 }
