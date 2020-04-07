32edb67aeb499ef5b46103e1efc6bfbb6ac747f9
abrie <abrie@users.noreply.github.com>
2020-04-07 14:24:41 -0400

    Require numeric inputs have a value.

--- src/components/Form/FormNumericInput.tsx
+++ src/components/Form/FormNumericInput.tsx
@@ -56,6 +56,7 @@ export function FormNumericInput<T>({
                       max={max}
                       step={step}
                       value={value}
+                      required
                       onChange={(newValue) => setFieldValue(identifier, newValue)}
                       style={{input:{width:'100%',padding:"1ex"}}}
                     />
