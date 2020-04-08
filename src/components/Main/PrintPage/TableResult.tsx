bef03ebabc2616cb3d8ac44b98c53aae09c2bed5
Nicholas Noll <nnoll523@gmail.com>
2020-04-08 16:26:14 -0700

    added error bars to print preview page

--- src/components/Main/PrintPage/TableResult.tsx
+++ src/components/Main/PrintPage/TableResult.tsx
@@ -15,12 +15,22 @@ const STEP = 7
 const dateFormat = (time: number) => moment(time).format('MMM DD YYYY')
 
 export default function TableResult({ result }: PropsType) {
-  const downSampled = result.trajectory.mean.reduce<ExportedTimePoint[]>((acc, curr, i) => {
-    if (i % STEP === 0) {
-      return [...acc, curr]
-    }
-    return acc
-  }, [])
+  const downSampled = {
+    mean: result.trajectory.mean.reduce<ExportedTimePoint[]>((acc, curr, i) => {
+      if (i % STEP === 0) {
+        return [...acc, curr]
+      }
+      return acc
+    }, []),
+
+    variance: result.trajectory.variance.reduce<ExportedTimePoint[]>((acc, curr, i) => {
+      if (i % STEP === 0) {
+        return [...acc, curr]
+      }
+      return acc
+    }, []),
+  }
+
   return (
     <div className="tableResult">
       <table>
@@ -34,20 +44,24 @@ export default function TableResult({ result }: PropsType) {
           </tr>
         </thead>
         <tbody>
-          {downSampled.map((line) => (
+          {downSampled.mean.map((line, i) => (
             <tr key={line.time}>
               <td>{dateFormat(line.time)}</td>
               <td style={{ backgroundColor: chroma(colors.severe).alpha(0.1).hex() }}>
-                {Math.round(line.current.severe.total)}
+                {Math.round(line.current.severe.total)}±
+                {Math.round(Math.sqrt(downSampled.variance[i].current.severe.total))}
               </td>
               <td style={{ backgroundColor: chroma(colors.critical).alpha(0.1).hex() }}>
-                {Math.round(line.current.critical.total)}
+                {Math.round(line.current.critical.total)}±
+                {Math.round(Math.sqrt(downSampled.variance[i].current.critical.total))}
               </td>
               <td style={{ backgroundColor: chroma(colors.recovered).alpha(0.1).hex() }}>
-                {Math.round(line.cumulative.recovered.total)}
+                {Math.round(line.cumulative.recovered.total)}±
+                {Math.round(Math.sqrt(downSampled.variance[i].cumulative.recovered.total))}
               </td>
               <td style={{ backgroundColor: chroma(colors.fatality).alpha(0.1).hex() }}>
-                {Math.round(line.cumulative.fatality.total)}
+                {Math.round(line.cumulative.fatality.total)}±
+                {Math.round(Math.sqrt(downSampled.variance[i].cumulative.fatality.total))}
               </td>
             </tr>
           ))}
