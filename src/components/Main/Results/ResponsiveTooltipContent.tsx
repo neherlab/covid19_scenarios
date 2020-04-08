5a58f047186a65c22abd4c77c787bba3b6f4605b
Nicholas Noll <nnoll523@gmail.com>
2020-04-08 11:03:33 -0700

    added error bars to tool tip

--- src/components/Main/Results/ResponsiveTooltipContent.tsx
+++ src/components/Main/Results/ResponsiveTooltipContent.tsx
@@ -5,6 +5,7 @@ import './ResponsiveTooltipContent.scss'
 interface TooltipItem {
   name: string
   value: string | number | ReactNode
+  error?: string | number | ReactNode
   color: string
 }
 
@@ -16,25 +17,48 @@ interface TooltipContentProps {
   labelFormatter?: Function
 }
 
-function ToolTipContentItem({ name, value, color }: TooltipItem) {
-  return (
-    <div style={{ color }} className="responsive-tooltip-content-item">
-      {name}
-      <div className="responsive-tooltip-content-placeholder" />
-      {value}
-    </div>
-  )
+function ToolTipContentItem({ name, value, error, color }: TooltipItem) {
+  if (error) {
+    return (
+      <div style={{ color }} className="responsive-tooltip-content-item">
+        {name}
+        <div className="responsive-tooltip-content-placeholder" />
+        <div>
+          {value} ± {error}
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={{ color }} className="responsive-tooltip-content-item">
+        {name}
+        <div className="responsive-tooltip-content-placeholder" />
+        {value}
+      </div>
+    )
+  }
 }
 
 export function ResponsiveTooltipContent({ active, payload, label, formatter, labelFormatter }: TooltipContentProps) {
   const formattedLabel = labelFormatter && label ? labelFormatter(label) : label
   const formatNumber = formatter
 
-  const essentialPayload = payload.map((payloadItem) => ({
-    name: payloadItem.name,
-    color: payloadItem.color || '#bbbbbb',
-    value: formatter ? formatNumber(payloadItem.value, '', '', 0) : payloadItem.value,
-  }))
+  const uncertainty: Record<string, number> = {}
+  payload.forEach((item) => {
+    if (item.name && item.name.includes(' uncertainty')) {
+      const key = item.name.replace(' uncertainty', '')
+      uncertainty[key] = (item.value[1] - item.value[0]) / 2.0
+    }
+  })
+
+  const essentialPayload = payload
+    .map((payloadItem) => ({
+      name: payloadItem.name,
+      color: payloadItem.color || '#bbbbbb',
+      value: formatter ? formatNumber(payloadItem.value, '', '', 0) : payloadItem.value,
+      error: payloadItem.name in uncertainty ? formatNumber(uncertainty[payloadItem.name], '', '', 0) : undefined,
+    }))
+    .filter((payloadItem) => (payloadItem.name ? !payloadItem.name.includes('uncertainty') : true))
 
   const left = payload.length > 1 ? essentialPayload.slice(0, Math.floor(payload.length / 2)) : payload
   const right = payload.length > 1 ? essentialPayload.slice(Math.floor(payload.length / 2), payload.length - 1) : []
@@ -46,12 +70,12 @@ export function ResponsiveTooltipContent({ active, payload, label, formatter, la
         <div className="responsive-tooltip-content">
           <div>
             {left.map((item, idx) => (
-              <ToolTipContentItem key={idx} name={item.name} value={item.value} color={item.color} />
+              <ToolTipContentItem key={idx} name={item.name} value={item.value} error={item.error} color={item.color} />
             ))}
           </div>
           <div>
             {right.map((item, idx) => (
-              <ToolTipContentItem key={idx} name={item.name} value={item.value} color={item.color} />
+              <ToolTipContentItem key={idx} name={item.name} value={item.value} error={item.error} color={item.color} />
             ))}
           </div>
         </div>
