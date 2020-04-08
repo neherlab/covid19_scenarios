e90975e79fc7b8d0dc47cceded7a65889ab2edec
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-04-08 17:10:20 +0200

    feat: adjust form schema for current layout

--- src/components/Main/validation/schema.ts
+++ src/components/Main/validation/schema.ts
@@ -4,6 +4,7 @@ import i18next from 'i18next'
 
 import { ageDistributionNames } from '../state/countryAgeDistributionData'
 import { CUSTOM_COUNTRY_NAME } from '../state/state'
+import { DateRange } from '../../../.generated/types'
 
 const countries = [...ageDistributionNames, CUSTOM_COUNTRY_NAME]
 
@@ -14,15 +15,21 @@ export const schema = yup.object().shape({
   population: yup.object().shape({
     populationServed: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
-    country: yup.string().required(MSG_REQUIRED).oneOf(countries, i18next.t('No such country in our data')),
+    country: yup.string().required(MSG_REQUIRED).oneOf(countries, i18next.t('No such region in our data')),
 
     suspectedCasesToday: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
-    importsPerDay: yup.number().required(MSG_REQUIRED),
+    importsPerDay: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
+
+    cases: yup.string().required(MSG_REQUIRED).oneOf(countries, i18next.t('No such region in our data')),
+
+    hospitalBeds: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
+
+    ICUBeds: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
   }),
 
   epidemiological: yup.object().shape({
-    r0: yup.number().required(MSG_REQUIRED),
+    r0: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
     latencyTime: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
@@ -30,26 +37,44 @@ export const schema = yup.object().shape({
 
     lengthHospitalStay: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
-    seasonalForcing: yup.number().required(MSG_REQUIRED),
+    lengthICUStay: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
+
+    seasonalForcing: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
+
+    overflowSeverity: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE),
 
     peakMonth: yup.number().required(MSG_REQUIRED).min(0, MSG_NON_NEGATIVE).max(11),
   }),
 
-  simulation: yup.object().shape({
-    numberStochasticRuns: yup
-      .number()
-      // .required(MSG_REQUIRED)
-      .min(0, MSG_NON_NEGATIVE)
-      .max(100, i18next.t('too many stochastic trajectories will slow things down')),
-
-    tMin: yup.date(),
-    // .required(MSG_REQUIRED),
-
-    tMax: yup.date(),
-    // .required(MSG_REQUIRED),
+  containment: yup.object().shape({
+    mitigationIntervals: yup.array().of(
+      yup.object({
+        color: yup.string().required(),
+        id: yup.string().required(),
+        mitigationValue: yup.number().min(0, MSG_NON_NEGATIVE).max(100).required(),
+        name: yup.string().required(),
+        timeRange: yup
+          .object({
+            tMin: yup.date().required(MSG_REQUIRED),
+            tMax: yup.date().required(MSG_REQUIRED),
+          })
+          .required(),
+      }),
+    ),
   }),
 
-  // serialInterval: yup.number().required(REQUIRED),
+  simulation: yup.object().shape({
+    // numberStochasticRuns: yup
+    //   .number()
+    //   .required(MSG_REQUIRED)
+    //   .min(0, MSG_NON_NEGATIVE)
+    //   .max(100, i18next.t('too many stochastic trajectories will slow things down')),
 
-  // tMax: yup.string().required(REQUIRED),
+    simulationTimeRange: yup
+      .object({
+        tMin: yup.date().required(MSG_REQUIRED),
+        tMax: yup.date().required(MSG_REQUIRED),
+      })
+      .required(),
+  }),
 })
