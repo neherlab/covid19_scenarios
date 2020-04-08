b313e74d368cafa61996fd4ad80b816e39070a17
Bruno RZN <bruno@velours.io>
2020-04-08 17:49:47 +0200

    feat: add TableResult component
    used for printing

---
+++ src/components/Main/PrintPage/TableResult.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import moment from 'moment'
+import { AlgorithmResult, ExportedTimePoint } from '../../../algorithms/types/Result.types'
+import { colors } from '../Results/DeterministicLinePlot'
+
+interface PropsType {
+  result: AlgorithmResult
+}
+
+const STEP = 7
+
+// deaths, ICU, hospitalized, recovered
+
+/*
+
+deaths = cumulative.fatality ?
+ICU = current.critical ?
+hospitalize = current.severe, sumulative.hospitalized ?
+recovered = cumulative.recovered ?
+
+
+*/
+
+const dateFormat = (time: number) => moment(time).format('MMM Do YY')
+
+export default function TableResult({ result }: PropsType) {
+  const downSampled = result.deterministic.trajectory.reduce<ExportedTimePoint[]>((acc, curr, i) => {
+    if (i % STEP === 0) {
+      return [...acc, curr]
+    }
+    return acc
+  }, [])
+  return (
+    <div className="tableResult">
+      <table>
+        <thead>
+          <tr>
+            <td>date</td>
+            <td style={{ backgroundColor: colors.severe }}>hospitalized</td>
+            <td style={{ backgroundColor: colors.critical }}>ICU</td>
+            <td style={{ backgroundColor: colors.recovered }}>recovered</td>
+            <td style={{ backgroundColor: colors.fatality }}>deaths</td>
+          </tr>
+        </thead>
+        <tbody>
+          {downSampled.map((line) => (
+            <tr key={line.time}>
+              <td>{dateFormat(line.time)}</td>
+              <td style={{ backgroundColor: colors.severe }}>{Math.round(line.current.severe.total)}</td>
+              <td style={{ backgroundColor: colors.critical }}>{Math.round(line.current.critical.total)}</td>
+              <td style={{ backgroundColor: colors.recovered }}>{Math.round(line.cumulative.recovered.total)}</td>
+              <td style={{ backgroundColor: colors.fatality }}>{Math.round(line.cumulative.fatality.total)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
