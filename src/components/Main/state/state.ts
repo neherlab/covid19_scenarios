d28437f59ec09cb979aa0fa1cec27a4595c3a3d1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-04-08 20:46:52 +0200

    feat: US is now default scenario

--- src/components/Main/state/state.ts
+++ src/components/Main/state/state.ts
@@ -11,7 +11,7 @@ export interface State {
 }
 
 // TODO: Add a default category to export
-export const DEFAULT_OVERALL_SCENARIO_NAME = 'CHE-Basel-Stadt'
+export const DEFAULT_OVERALL_SCENARIO_NAME = 'United States of America'
 export const CUSTOM_SCENARIO_NAME = i18next.t('Custom')
 export const CUSTOM_COUNTRY_NAME = 'Custom'
 
