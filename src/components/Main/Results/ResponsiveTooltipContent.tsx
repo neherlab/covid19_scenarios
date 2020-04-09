2ce5fd96ba0863a18cdbc181d2718327b931436f
Gavin Jefferies <gavin@runtime.io>
2020-04-09 15:29:37 -0700

    have a consistent tooltip

--- src/components/Main/Results/ResponsiveTooltipContent.tsx
+++ src/components/Main/Results/ResponsiveTooltipContent.tsx
@@ -1,22 +1,31 @@
 import React, { ReactNode } from 'react'
 import { TooltipProps } from 'recharts'
+import { useTranslation } from 'react-i18next'
+
+import { linesToPlot, translateLinesToPlot } from './LinePlotCommon'
+
 import './ResponsiveTooltipContent.scss'
 
 export function ResponsiveTooltipContent({ active, payload, label, formatter, labelFormatter }: TooltipProps) {
+  const { t } = useTranslation()
+
   if (!active || !label || !payload || payload.length <= 2) {
     return null
   }
 
   const formattedLabel = labelFormatter && label !== undefined ? labelFormatter(label) : '-'
 
-  const formattedPayload = payload.map((item, idx) => ({
-    name: item.name,
-    color: item.color || '#bbbbbb',
-    value: formatter ? formatter(item.value as string | number, item.name, item, idx) : item.value,
-  }))
+  const tooltipItems = translateLinesToPlot(t, linesToPlot).map((lineToPlot, idx) => {
+    const item = payload.find((item) => item.dataKey === lineToPlot.key) || { ...lineToPlot, value: 0 }
+    return {
+      name: item.name,
+      color: item.color || '#bbbbbb',
+      value: formatter ? formatter(item.value as string | number, item.name, item, idx) : item.value,
+    }
+  })
 
-  const left = payload.length > 1 ? formattedPayload.slice(0, Math.floor(payload.length / 2)) : formattedPayload
-  const right = payload.length > 1 ? formattedPayload.slice(Math.floor(payload.length / 2), payload.length - 1) : []
+  const left = tooltipItems.slice(0, Math.floor(payload.length / 2))
+  const right = tooltipItems.slice(Math.floor(payload.length / 2), payload.length - 1)
 
   return (
     <div className="responsive-tooltip-content-base">
