46864e7ce9d59ead2ae6d275bba4a3f5f5acc2e2
abrie <abrie@users.noreply.github.com>
2020-04-09 14:13:14 -0400

    Use InputGroup to structure NumericInput.

--- src/components/Form/NumericInput.tsx
+++ src/components/Form/NumericInput.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md'
 
 import { Field } from 'formik'
-import { Col, Row, Input, Button, ButtonGroup } from 'reactstrap'
+import { Col, InputGroup, InputGroupAddon, Row, Input, Button, ButtonGroup } from 'reactstrap'
 
 export interface NumericInputProps<T> {
   identifier: string
@@ -31,47 +31,44 @@ export default function NumericInput<T>({
   }
 
   const spinnerButtonStyle = {
-    'font-size': '0rem',
+    'fontSize': '0rem',
     'padding': '0',
-    'max-width': '1.5rem',
-    'max-height': '1.2rem',
+    'maxWidth': '1.5rem',
+    'maxHeight': '1.2rem',
+    'margin': '0',
   }
 
   const pattern = "^[-]?[0-9]+([,\.][0-9]+)?"
 
   return (
-    <Field name={identifier}>
-      {({ field, form: { setFieldValue, setFieldTouched } }: FieldProps<number>) => (
-        <Row noGutters>
-          <Col xs={10} sm={10} md={10} lg={10} xl={10}>
+      <Field name={identifier}>
+        {({ field, form: { setFieldValue, setFieldTouched } }: FieldProps<number>) => (
+          <InputGroup>
             <Input type="text" inputMode="numeric" pattern={pattern} aria-label={label} {...field} />
-          </Col>
-          <Col xs={2} sm={2} md={2} lg={2} xl={2}>
-            <ButtonGroup vertical>
-              <Button
-                className="py-0"
-                style={spinnerButtonStyle}
-                onClick={() => {
-                  setFieldTouched(field.name, true)
-                  setFieldValue(field.name, increase(field.value, step))
-                }}
-              >
-                <MdArrowDropUp size={20} />
-              </Button>
-              <Button
-                className="py-0"
-                style={spinnerButtonStyle}
-                onClick={() => {
-                  setFieldTouched(field.name, true)
-                  setFieldValue(field.name, decrease(field.value, step))
-                }}
-              >
-                <MdArrowDropDown size={20} />
-              </Button>
-            </ButtonGroup>
-          </Col>
-        </Row>
-      )}
-    </Field>
+            <InputGroupAddon addonType="append">
+              <ButtonGroup vertical>
+                <Button
+                  style={spinnerButtonStyle}
+                  onClick={() => {
+                    setFieldTouched(field.name, true)
+                    setFieldValue(field.name, increase(field.value, step))
+                  }}
+                >
+                  <MdArrowDropUp size={20} />
+                </Button>
+                <Button
+                  style={spinnerButtonStyle}
+                  onClick={() => {
+                    setFieldTouched(field.name, true)
+                    setFieldValue(field.name, decrease(field.value, step))
+                  }}
+                >
+                  <MdArrowDropDown size={20} />
+                </Button>
+              </ButtonGroup>
+            </InputGroupAddon>
+          </InputGroup>
+        )}
+      </Field>
   )
 }
