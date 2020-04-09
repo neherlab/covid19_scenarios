84fbb9754611383013ed10d533535dbb50b7a415
abrie <abrie@users.noreply.github.com>
2020-04-09 09:52:20 -0400

    Refactor NumericInput from FormNumericInput in module.

--- src/components/Form/FormNumericInput.tsx
+++ src/components/Form/FormNumericInput.tsx
@@ -8,6 +8,7 @@ import { Field, FormikErrors, FormikTouched } from 'formik'
 import { Col, FormGroup, Row, Input, Button, ButtonGroup } from 'reactstrap'
 
 import FormLabel from './FormLabel'
+import NumericInput from './NumericInput'
 
 export interface FormNumericInputProps<T> {
   identifier: string
@@ -32,28 +33,6 @@ export function FormNumericInput<T>({
   const errorMessage = _.get(errors, identifier)
   const showError = errorMessage && isTouched
 
-  const toPrecision = (value) =>
-    Math.round((value + Number.EPSILON) * Math.pow(10, precision)) / Math.pow(10, precision)
-
-  const increase = (value, step) => {
-    const parsed = parseFloat(value)
-    const result = parsed + step
-    return toPrecision(result)
-  }
-
-  const decrease = (value, step) => {
-    const parsed = parseFloat(value)
-    const result = parsed - step
-    return toPrecision(result)
-  }
-
-  const spinnerButtonStyle = {
-    'font-size': '0rem',
-    'padding': '0',
-    'max-width': '1.5rem',
-    'max-height': '1.2rem',
-  }
-
   return (
     <Field>
       {() => (
@@ -63,39 +42,7 @@ export function FormNumericInput<T>({
               <FormLabel identifier={identifier} label={label} help={help} />
             </Col>
             <Col xl={5}>
-              <Field name={identifier}>
-                {({ field, form: { setFieldValue, setFieldTouched } }: FieldProps<number>) => (
-                  <Row noGutters>
-                    <Col xs={10} sm={10} md={10} lg={10} xl={10}>
-                      <Input type="text" inputMode="numeric" pattern="^[-]?[0-9]+([,\.][0-9]+)?" aria-label={label} {...field} />
-                    </Col>
-                    <Col xs={2} sm={2} md={2} lg={2} xl={2}>
-                      <ButtonGroup vertical>
-                        <Button
-                          className="py-0"
-                          style={spinnerButtonStyle}
-                          onClick={() => {
-                            setFieldTouched(field.name, true)
-                            setFieldValue(field.name, increase(field.value, step))
-                          }}
-                        >
-                          <MdArrowDropUp size={20} />
-                        </Button>
-                        <Button
-                          className="py-0"
-                          style={spinnerButtonStyle}
-                          onClick={() => {
-                            setFieldTouched(field.name, true)
-                            setFieldValue(field.name, decrease(field.value, step))
-                          }}
-                        >
-                          <MdArrowDropDown size={20} />
-                        </Button>
-                      </ButtonGroup>
-                    </Col>
-                  </Row>
-                )}
-              </Field>
+              <NumericInput identifier={identifier} label={label} step={step} precision={precision}/>
               {showError ? <div className="text-danger">{errorMessage}</div> : null}
             </Col>
           </Row>
---
+++ src/components/Form/NumericInput.tsx
@@ -0,0 +1,83 @@
+
+import React from 'react'
+
+import _ from 'lodash'
+
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md'
+
+import { Field, FormikErrors, FormikTouched } from 'formik'
+import { Col, FormGroup, Row, Input, Button, ButtonGroup } from 'reactstrap'
+
+import FormLabel from './FormLabel'
+
+export interface NumericInputProps<T> {
+  identifier: string
+  label: string
+  step?: number | string
+  precision?: number
+}
+
+export default function NumericInput<T>({
+  identifier,
+  label,
+  step,
+  precision,
+}: NumericInputProps<T>) {
+
+  const toPrecision = (value) =>
+    Math.round((value + Number.EPSILON) * Math.pow(10, precision)) / Math.pow(10, precision)
+
+  const increase = (value, step) => {
+    const parsed = parseFloat(value)
+    const result = parsed + step
+    return toPrecision(result)
+  }
+
+  const decrease = (value, step) => {
+    const parsed = parseFloat(value)
+    const result = parsed - step
+    return toPrecision(result)
+  }
+
+  const spinnerButtonStyle = {
+    'font-size': '0rem',
+    'padding': '0',
+    'max-width': '1.5rem',
+    'max-height': '1.2rem',
+  }
+return (
+              <Field name={identifier}>
+                {({ field, form: { setFieldValue, setFieldTouched } }: FieldProps<number>) => (
+                  <Row noGutters>
+                    <Col xs={10} sm={10} md={10} lg={10} xl={10}>
+                      <Input type="text" inputMode="numeric" pattern="^[-]?[0-9]+([,\.][0-9]+)?" aria-label={label} {...field} />
+                    </Col>
+                    <Col xs={2} sm={2} md={2} lg={2} xl={2}>
+                      <ButtonGroup vertical>
+                        <Button
+                          className="py-0"
+                          style={spinnerButtonStyle}
+                          onClick={() => {
+                            setFieldTouched(field.name, true)
+                            setFieldValue(field.name, increase(field.value, step))
+                          }}
+                        >
+                          <MdArrowDropUp size={20} />
+                        </Button>
+                        <Button
+                          className="py-0"
+                          style={spinnerButtonStyle}
+                          onClick={() => {
+                            setFieldTouched(field.name, true)
+                            setFieldValue(field.name, decrease(field.value, step))
+                          }}
+                        >
+                          <MdArrowDropDown size={20} />
+                        </Button>
+                      </ButtonGroup>
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+)
+}
