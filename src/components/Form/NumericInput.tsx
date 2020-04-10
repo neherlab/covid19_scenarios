b62a5aa2e6e3954d1235fdeb91e80380ebf2b92c
abrie <abrie@users.noreply.github.com>
2020-04-10 09:13:48 -0400

    Use ellipsis if number too large for display.

--- src/components/Form/NumericInput.tsx
+++ src/components/Form/NumericInput.tsx
@@ -38,13 +38,17 @@ export default function NumericInput<T>({
     'margin': '0',
   }
 
+  const inputStyle = {
+    'textOverflow':'ellipsis'
+  }
+
   const pattern = "^[-]?[0-9]+([,\.][0-9]+)?"
 
   return (
       <Field name={identifier}>
         {({ field, form: { setFieldValue, setFieldTouched } }: FieldProps<number>) => (
           <InputGroup>
-            <Input type="text" inputMode="numeric" pattern={pattern} aria-label={label} {...field} />
+            <Input type="text" inputMode="numeric" pattern={pattern} aria-label={label} style={inputStyle} {...field} />
             <InputGroupAddon addonType="append">
               <ButtonGroup vertical>
                 <Button
