d0999de803ffd69f6354e8e287914abf26056c1b
abrie <abrie@users.noreply.github.com>
2020-04-09 20:11:36 -0400

    New layout for MitigationTable.

--- src/components/Main/Mitigation/MitigationTable.tsx
+++ src/components/Main/Mitigation/MitigationTable.tsx
@@ -4,7 +4,7 @@ import _ from 'lodash'
 
 import { FastField, FieldArray, FieldArrayRenderProps, FormikErrors, FormikTouched, FormikValues } from 'formik'
 import { useTranslation } from 'react-i18next'
-import { Button, Row, Col, FormGroup } from 'reactstrap'
+import { Button, Row, Col, FormGroup, InputGroup } from 'reactstrap'
 
 import { FaTrash, FaPlus } from 'react-icons/fa'
 
@@ -43,40 +43,46 @@ export function MitigationTable({ mitigationIntervals, errors, touched }: Mitiga
     <Row>
       <Row>
         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <p>The presets for the mitigation and infections control measure below are currently just place holders. We are gathering this information at the moment. For the time being please adjust, add, and remove to match your community.</p>
+          <p>
+            The presets for the mitigation and infections control measure below are currently just place holders. We are
+            gathering this information at the moment. For the time being please adjust, add, and remove to match your
+            community.
+          </p>
           <p>Each measure consists of name, start/end date, and an effectiveness in %.</p>
         </Col>
       </Row>
       <Row noGutters>
-          <FieldArray
-            name="containment.mitigationIntervals"
-            render={(arrayHelpers) => (
+        <FieldArray
+          name="containment.mitigationIntervals"
+          render={(arrayHelpers) => (
+            <>
               <Row noGutters>
                 {mitigationIntervals.map((interval: MitigationInterval, index: number) => (
-                    <Col key={interval.id} xs={12} sm={12} md={12} lg={12} xl={12}>
-                      <MitigationIntervalComponent
-                        interval={interval}
-                        index={index}
-                        arrayHelpers={arrayHelpers}
-                        errors={errors}
-                        touched={touched}
-                      />
-                    </Col>
-                  ))}
+                  <Col key={interval.id} xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <MitigationIntervalComponent
+                      interval={interval}
+                      index={index}
+                      arrayHelpers={arrayHelpers}
+                      errors={errors}
+                      touched={touched}
+                    />
+                  </Col>
+                ))}
               </Row>
-            )}
-          />
-      </Row>
-      <Row noGutters>
-        <Button
-          type="button"
-          onClick={() => {
-            const interval = suggestNextMitigationInterval(mitigationIntervals)
-            arrayHelpers.push(interval)
-          }}
-        >
-          {t('Add')} <FaPlus />
-        </Button>
+              <Row noGutters className="pl-2">
+                <Button
+                  type="button"
+                  onClick={() => {
+                    const interval = suggestNextMitigationInterval(mitigationIntervals)
+                    arrayHelpers.push(interval)
+                  }}
+                >
+                  {t('Add')} <FaPlus />
+                </Button>
+              </Row>
+            </>
+          )}
+        />
       </Row>
     </Row>
   )
@@ -90,13 +96,7 @@ interface MitigationIntervalProps {
   touched?: FormikTouched<FormikValues>
 }
 
-function MitigationIntervalComponent({
-  index,
-  interval,
-  arrayHelpers,
-  errors,
-  touched,
-}: MitigationIntervalProps) {
+function MitigationIntervalComponent({ index, interval, arrayHelpers, errors, touched }: MitigationIntervalProps) {
   const { t } = useTranslation()
 
   const nameError = getFormikError({
@@ -112,9 +112,21 @@ function MitigationIntervalComponent({
   })
 
   return (
-    <FormGroup className="w-100">
-      <Row noGutters>
-        <Col size="12" xs="12" sm="12" md="12" lg="12" xl="12">
+    <FormGroup className="w-100 p-2">
+      <Row noGutters style={{ background: '#ddd' }}>
+        <Col xs="12" sm="12" md="12" lg="12" xl="12">
+          <InputGroup className="align-items-center p-1 pr-1">
+            <MitigationDatePicker
+              identifier={`containment.mitigationIntervals[${index}].timeRange`}
+              value={interval.timeRange}
+              allowPast
+            />
+            <Button type="button" onClick={() => arrayHelpers.remove(index)} className="ml-auto">
+              <FaTrash />
+            </Button>
+          </InputGroup>
+        </Col>
+        <Col xs="12" sm="12" md="12" lg="12" xl="12" className="align-self-center pl-1 pr-1">
           <FastField
             className={`name form-control ${nameError ? 'border-danger' : ''}`}
             id={`containment.mitigationIntervals[${index}].name`}
@@ -122,26 +134,14 @@ function MitigationIntervalComponent({
             type="text"
           />
         </Col>
-        <Col xs="auto" sm="auto" md="auto" lg="5" xl="auto" className="align-self-center">
-          <MitigationDatePicker
-            identifier={`containment.mitigationIntervals[${index}].timeRange`}
-            value={interval.timeRange}
-            allowPast
-          />
-        </Col>
-        <Col xs="4" sm="5" md="6" lg="4" xl="6" className="align-self-center">
+        <Col xs="12" sm="12" md="12" lg="12" xl="12" className="align-self-center pl-1 pr-1 pb-1">
           <NumericInput
-            className="form-control h-100"
+            className="form-control"
             identifier={`containment.mitigationIntervals[${index}].mitigationValue`}
             name={`containment.mitigationIntervals[${index}].mitigationValue`}
             step={1}
             precision={2}
           />
-        </Col>
-        <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" className="ml-auto align-self-center">
-          <Button type="button" onClick={() => arrayHelpers.remove(index)}>
-            <FaTrash />
-          </Button>
         </Col>
       </Row>
       <Row>
