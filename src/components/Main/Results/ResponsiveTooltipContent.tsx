cec10f56d9d5e5ec26f2eb4650eef456b8fad134
Gavin Jefferies <gavin@runtime.io>
2020-04-09 20:55:59 -0700

    refactor: separate tooltips

--- src/components/Main/Results/ResponsiveTooltipContent.tsx
+++ src/components/Main/Results/ResponsiveTooltipContent.tsx
@@ -1,71 +1,20 @@
 import React, { ReactNode } from 'react'
 import { TooltipProps } from 'recharts'
-import { useTranslation } from 'react-i18next'
 
-import { linesToPlot, observationsToPlot, translatePlots, LineProps } from './LinePlotCommon'
+import { LineProps } from './ChartCommon'
 
 import './ResponsiveTooltipContent.scss'
 
-interface TooltipItem extends LineProps {
+export interface TooltipItem extends LineProps {
   value: React.ReactNode
-  displayUndefinedAs: number | string
 }
 
-interface ResponsiveTooltipContentProps extends TooltipProps {
-  valueFormatter: (value: number | string) => string
-  itemsToDisplay?: string[]
+export interface ResponsiveTooltipContentProps extends TooltipProps {
+  formattedLabel: React.ReactNode
+  tooltipItems: TooltipItem[]
 }
 
-export function ResponsiveTooltipContent({
-  active,
-  payload,
-  label,
-  valueFormatter,
-  labelFormatter,
-  itemsToDisplay,
-}: ResponsiveTooltipContentProps) {
-  const { t } = useTranslation()
-
-  if (!active || !label || !payload || payload.length <= 2) {
-    return null
-  }
-
-  const formattedLabel = labelFormatter && label !== undefined ? labelFormatter(label) : '-'
-
-  const tooltipItems: TooltipItem[] = []
-    .concat(
-      translatePlots(t, observationsToPlot).map((observationToPlot) => ({
-        ...observationToPlot,
-        displayUndefinedAs: '-',
-      })) as never,
-    )
-    .concat(
-      translatePlots(t, linesToPlot).map((lineToPlot) => ({
-        ...lineToPlot,
-        displayUndefinedAs: 0,
-      })) as never,
-    )
-    .filter(
-      (tooltipItem: TooltipItem): boolean =>
-        !itemsToDisplay || !!itemsToDisplay.find((itemKey) => itemKey === tooltipItem.key),
-    )
-    .map(
-      (tooltipItem: TooltipItem): TooltipItem => {
-        const payloadItem = payload.find((payloadItem) => payloadItem.dataKey === tooltipItem.key)
-
-        const value =
-          // eslint-disable-next-line unicorn/no-nested-ternary
-          payloadItem && payloadItem.value !== undefined
-            ? (payloadItem.value as string | number)
-            : tooltipItem.displayUndefinedAs
-
-        return {
-          ...tooltipItem,
-          value: valueFormatter ? valueFormatter(value) : value,
-        }
-      },
-    )
-
+export function ResponsiveTooltipContent({ formattedLabel, tooltipItems }: ResponsiveTooltipContentProps) {
   const left = tooltipItems.slice(0, Math.ceil(tooltipItems.length / 2))
   const right = tooltipItems.slice(Math.ceil(tooltipItems.length / 2))
 
