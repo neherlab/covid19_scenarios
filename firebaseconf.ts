5b70c931d7ecebebe70e5012f5f424278ab2a091
Mika Laaksonen <48655257+miksa1987@users.noreply.github.com>
2020-04-12 21:00:15 +0300

    Delete firebaseconf.ts

--- firebaseconf.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB_T7TXfC2cjOS3VgqGBd-Yn-7R_j2UzIk",
-  authDomain: "covid19-scenarios.firebaseapp.com",
-  databaseURL: "https://covid19-scenarios.firebaseio.com",
-  projectId: "covid19-scenarios",
-  storageBucket: "covid19-scenarios.appspot.com",
-  messagingSenderId: "880897171256",
-  appId: "1:880897171256:web:5b5a83a9d3045083d42bcd"
-}
