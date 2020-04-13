6e58dadf797b093aa533d2df65eac15826fd0178
Nils Ole Tippenhauer <noleti@gmx.de>
2020-04-13 12:04:25 +0200

    integrate additional data sources

--- data/getPopulationData.py
+++ data/getPopulationData.py
@@ -11,12 +11,51 @@ from datetime import datetime, timedelta
 from parsers.utils import sorted_date, parse_countries, stoi, store_data
 
 #  WHO Hospital data from https://gateway.euro.who.int/en/indicators/hfa_479-5061-number-of-acute-care-hospital-beds/
+# WHO data is from 2015
 URL_Hosp = "https://dw.euro.who.int/api/v3/export/download/a8cb2f20d3e74b75a38769fb44c2dc9b"
-URL_Pop  = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-"
+# eurostat data is from 2017
+URL_Hosp_Eurostat = "https://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tps00046"
+# hardcode a date for ecdc because the data is not daily anyways, and sometimes has bugs
+URL_Pop  = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-04-04.xlsx"
 URL_ICU = "http://dx.doi.org/10.1007/s00134-012-2627-8" # the paper where the numbers came from
 # other ICU source: https://doi.org/10.1097/CCM.0000000000004222, Critical Care Bed Capacity in Asian Countries and Regions Article in Critical Care Medicine · January 2020
 URL_ICU_ASIA = "https://doi.org/10.1097/CCM.0000000000004222" # the paper where the numbers came from
+
 cols = ['name', 'populationServed', 'ageDistribution', 'hospitalBeds', 'ICUBeds', 'hemisphere', 'srcPopulation', 'srcHospitalBeds','srcICUBeds']
+
+def update_hosp_data(hospData, popData):
+    countries = parse_countries(2)
+    newData = {}
+    # OECD, only in percentages of population, up to around 2016
+    with open('hospital-data/hospital_capacity_oecd.csv', 'r') as fd:
+        rdr = csv.reader(fd, delimiter=',')
+        hdr = next(rdr)
+        
+        for row in rdr:
+            country   = row[0].strip()
+            if not country in countries:
+                print(f'{country} not found in countries')
+            else:
+                country = countries[country]
+            if not country in hospData:
+                if not country in newData:
+                    newData[country] = {}
+                newData[country][row[2]] = float(row[6])
+
+    # newData contains latest=newest entries only now, for countries we need to add
+    for country in newData:
+        if country in popData:
+            if 'ACUTE' in newData[country]:
+                if not country in hospData:
+                    hospData[country] = {}
+                hospData[country]['hospitalBeds'] = int(popData[country]['populationServed']/1000*newData[country]['ACUTE'])
+                hospData[country]['srcHospitalBeds'] = "Computed from acute care OECD numbers and population size"
+            elif 'TOT' in newData[country]:
+                if not country in hospData:
+                    hospData[country] = {}
+                hospData[country]['hospitalBeds'] = int(popData[country]['populationServed']/1000*newData[country]['TOT'])
+                hospData[country]['srcHospitalBeds'] = "Computed from total bed OECD numbers and population size"
+    return hospData
 
 def retrieve_hosp_data():
     countries = parse_countries(2)
@@ -51,20 +90,33 @@ def retrieve_hosp_data():
 
         for row in rdr:
             country   = row[0].strip()
-            hospData[country] = {'hospitalBeds': stoi(row[1]), 'srcHospitalBeds': URL_ICU_ASIA}
+            if stoi(row[1]):
+                hospData[country] = {'hospitalBeds': stoi(row[1]), 'srcHospitalBeds': URL_ICU_ASIA}
 
+    # Eurostat, newer than from WHO (2017 vs 2015)
+    with open('hospital-data/ICU_eurostat.tsv', 'r') as fd:
+        rdr = csv.reader(fd, delimiter='\t')
+        hdr = next(rdr)
+
+        for row in rdr:
+            country   = row[0].strip()
+            if not country in hospData:
+                hospData[country] = {'hospitalBeds': stoi(row[1]), 'srcHospitalBeds': URL_Hosp_Eurostat}
+            else:
+                print(f'found new data for {country}: old: {hospData[country]["hospitalBeds"]}, new: {stoi(row[1])}')
+                hospData[country] = {'hospitalBeds': stoi(row[1]), 'srcHospitalBeds': URL_Hosp_Eurostat}
+
+            
     return hospData
 
 def retrieve_pop_data():
     countries = parse_countries(2)
     countries1 = parse_countries(1)
-    #countries['UK'] = countries['GB']
     countries['XKX'] = countries['RKS']
+    countries1['BLM'] = countries1['BL']
     popData = {}
 
-    yesterday = datetime.today() - timedelta(days=1)
-    date = yesterday.strftime("%Y-%m-%d")
-    file_name, headers = urlretrieve(URL_Pop+date+".xlsx")
+    file_name, headers = urlretrieve(URL_Pop)
 
     workbook = xlrd.open_workbook(file_name)
 
@@ -147,6 +199,8 @@ if __name__ == "__main__":
     icuData = retrieve_icu_data()
     popData = retrieve_pop_data()
 
+    hospData = update_hosp_data(hospData, popData)
+    
     newData = {}
     for d in hospData:
         if not d in newData:
@@ -174,14 +228,14 @@ if __name__ == "__main__":
                 if not 'populationServed' in newData[n]:
                     newData[n]['populationServed'] = oldData[n]['populationServed']
                     newData[n]['srcPopulation'] = oldData[n]['srcPopulation']
-                elif oldData[n]['srcPopulation'] and not oldData[n]['srcPopulation']== 'None':
-                    print(f'Ignoring old pop source {oldData[n]["srcPopulation"]}')
-                    print(f'old val was {oldData[n]["populationServed"]}, new val is {newData[n]["populationServed"]}')
+                #elif oldData[n]['srcPopulation'] and not oldData[n]['srcPopulation']== 'None':
+                #    print(f'Ignoring old pop source {oldData[n]["srcPopulation"]}')
+                #    print(f'old val was {oldData[n]["populationServed"]}, new val is {newData[n]["populationServed"]}')
                 if not 'hospitalBeds' in newData[n]:
                     newData[n]['hospitalBeds'] = oldData[n]['hospitalBeds']
                     newData[n]['srcHospitalBeds'] = oldData[n]['srcHospitalBeds']
-                elif oldData[n]['srcHospitalBeds'] and not oldData[n]['srcHospitalBeds']== 'None':
-                    print(f'Ignoring old hosp source {oldData[n]["srcPopulation"]}')
+                #elif oldData[n]['srcHospitalBeds'] and not oldData[n]['srcHospitalBeds']== 'None':
+                #    print(f'Ignoring old hosp source {oldData[n]["srcPopulation"]}')
                 if not 'ICUBeds' in newData[n]:
                     newData[n]['ICUBeds'] = oldData[n]['ICUBeds']
                     newData[n]['srcICUBeds'] = oldData[n]['srcICUBeds']
@@ -203,8 +257,8 @@ if __name__ == "__main__":
     for k in toDel:
         del newData[k]
     print(f'{i} entries are complete')
-    with open('popData.tsv', 'w') as fd:
-        wtr = csv.writer(fd, delimiter='\t')
+    with open('populationData.tsv', 'w', newline="") as fd:
+        wtr = csv.writer(fd, delimiter='\t', lineterminator='\n')
         wtr.writerow(cols)
         rows = []
         for d in newData:
