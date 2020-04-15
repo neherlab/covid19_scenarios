84e0581915e1f1ddfb8fed83e307d25c5867502b
Nicholas Noll <nnoll523@gmail.com>
2020-04-15 13:24:38 -0700

    fix: remove deprecated mitigation results

--- src/algorithms/run.ts
+++ src/algorithms/run.ts
@@ -1,7 +1,6 @@
 import { AgeDistribution } from '../.generated/types'
 import { SeverityTableRow } from '../components/Main/Scenario/ScenarioTypes'
 import { AllParamsFlat } from './types/Param.types'
-import { TimeSeries } from './types/TimeSeries.types'
 import { AlgorithmResult, SimulationTimePoint, ExportedTimePoint } from './types/Result.types'
 
 import { getPopulationParams, initializePopulation } from './initialize'
@@ -31,7 +30,6 @@ export async function run(
   const modelParamsArray = getPopulationParams(params, severity, ageDistribution)
 
   const trajectories: ExportedTimePoint[][] = []
-  const containments: TimeSeries[] = []
 
   modelParamsArray.forEach((modelParams) => {
     const population = initializePopulation(modelParams.populationServed, initialCases, tMin, ageDistribution)
@@ -48,12 +46,6 @@ export async function run(
     }
     const trajectory = simulate(population, identity)
     trajectories.push(trajectory)
-    containments.push(
-      trajectory.map((d) => ({
-        t: d.time,
-        y: modelParams.rate.infection(d.time),
-      })),
-    )
   })
 
   const mean = meanTrajectory(trajectories)
@@ -84,11 +76,6 @@ export async function run(
       upper: mean.map((m, i) => mulTP(m, sdev[i])),
       lower: mean.map((m, i) => divTP(m, sdev[i])),
       percentile: {},
-    },
-    mitigation: {
-      mean: [],
-      lower: [],
-      upper: [],
     },
     R0: {
       mean: meanR0,
