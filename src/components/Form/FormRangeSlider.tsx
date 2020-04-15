c136f87035943b0f53f0b499b0cb09f43ef6e420
Nicholas Noll <nnoll523@gmail.com>
2020-04-15 14:05:03 -0700

    chore: move declarations to be consistent with C stylings

--- src/components/Form/FormRangeSlider.tsx
+++ src/components/Form/FormRangeSlider.tsx
@@ -7,90 +7,6 @@ import { Range, getTrackBackground } from 'react-range'
 import { Col, FormGroup, Row } from 'reactstrap'
 import FormLabel from './FormLabel'
 
-export interface RangeSliderProps<T> {
-  identifier: string
-  label?: string
-  help?: string | React.ReactNode
-  step: number
-  min: number
-  max: number
-
-  errors?: FormikErrors<T>
-  touched?: FormikTouched<T>
-}
-
-export function RangeSlider<T>({ identifier, label, help, step, min, max, errors, touched }: RangeSliderProps<T>) {
-  const isTouched = _.get(touched, identifier)
-  const errorMessage = _.get(errors, identifier)
-  const showError = errorMessage && isTouched
-  const borderDanger = showError ? 'border-danger' : ''
-
-  function validate(value?: number[]) {
-    let error
-    if (!value) {
-      return error
-    }
-
-    if (value.length !== 2) {
-      error = `The input length must be 2, found ${value.length}`
-    }
-    if (value[0] > value[1]) {
-      error = `The lower bound ${value[0]} cannot be greater than than the upper bound ${value[1]}`
-    }
-    if (min && value[0] < min) {
-      error = `The lower bound cannot be less than ${min}`
-    } else if (max && value[1] > max) {
-      error = `The upper bound cannot be greater than ${max}`
-    }
-
-    return error
-  }
-
-  return (
-    <Field name={identifier} className={`form-control ${borderDanger}`} validate={validate}>
-      {({ field: { value }, form: { setFieldValue } }: FieldProps<number[]>) => {
-        const exportInternalState = (value: number[]) => {
-          setFieldValue(identifier, value)
-        }
-
-        if (label) {
-          return (
-            <FormGroup className="my-0">
-              <Row noGutters>
-                <Col xl={7}>
-                  <FormLabel identifier={identifier} label={label} help={help} />
-                </Col>
-                <Col xl={5}>
-                  <BaseRangeSlider
-                    value={[value[0], value[1]]}
-                    exportInternalState={exportInternalState}
-                    step={step}
-                    min={min}
-                    max={max}
-                  />
-                </Col>
-              </Row>
-            </FormGroup>
-          )
-        }
-        return (
-          <FormGroup className="my-0">
-            <Row noGutters>
-              <BaseRangeSlider
-                value={[value[0], value[1]]}
-                exportInternalState={exportInternalState}
-                step={step}
-                min={min}
-                max={max}
-              />
-            </Row>
-          </FormGroup>
-        )
-      }}
-    </Field>
-  )
-}
-
 export interface BaseRangeSliderProps {
   value: [number, number]
   step: number
@@ -180,5 +96,88 @@ function BaseRangeSlider({ value, exportInternalState, step, min, max }: BaseRan
         </div>
       )}
     />
+  )
+}
+export interface RangeSliderProps<T> {
+  identifier: string
+  label?: string
+  help?: string | React.ReactNode
+  step: number
+  min: number
+  max: number
+
+  errors?: FormikErrors<T>
+  touched?: FormikTouched<T>
+}
+
+export function RangeSlider<T>({ identifier, label, help, step, min, max, errors, touched }: RangeSliderProps<T>) {
+  const isTouched = _.get(touched, identifier)
+  const errorMessage = _.get(errors, identifier)
+  const showError = errorMessage && isTouched
+  const borderDanger = showError ? 'border-danger' : ''
+
+  function validate(value?: number[]) {
+    let error
+    if (!value) {
+      return error
+    }
+
+    if (value.length !== 2) {
+      error = `The input length must be 2, found ${value.length}`
+    }
+    if (value[0] > value[1]) {
+      error = `The lower bound ${value[0]} cannot be greater than than the upper bound ${value[1]}`
+    }
+    if (min && value[0] < min) {
+      error = `The lower bound cannot be less than ${min}`
+    } else if (max && value[1] > max) {
+      error = `The upper bound cannot be greater than ${max}`
+    }
+
+    return error
+  }
+
+  return (
+    <Field name={identifier} className={`form-control ${borderDanger}`} validate={validate}>
+      {({ field: { value }, form: { setFieldValue } }: FieldProps<number[]>) => {
+        const exportInternalState = (value: number[]) => {
+          setFieldValue(identifier, value)
+        }
+
+        if (label) {
+          return (
+            <FormGroup className="my-0">
+              <Row noGutters>
+                <Col xl={7}>
+                  <FormLabel identifier={identifier} label={label} help={help} />
+                </Col>
+                <Col xl={5}>
+                  <BaseRangeSlider
+                    value={[value[0], value[1]]}
+                    exportInternalState={exportInternalState}
+                    step={step}
+                    min={min}
+                    max={max}
+                  />
+                </Col>
+              </Row>
+            </FormGroup>
+          )
+        }
+        return (
+          <FormGroup className="my-0">
+            <Row noGutters>
+              <BaseRangeSlider
+                value={[value[0], value[1]]}
+                exportInternalState={exportInternalState}
+                step={step}
+                min={min}
+                max={max}
+              />
+            </Row>
+          </FormGroup>
+        )
+      }}
+    </Field>
   )
 }
