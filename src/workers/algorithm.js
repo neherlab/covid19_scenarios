c682f64cfa0204a0c513abe8ef444875d4a42287
abrie <abrie@users.noreply.github.com>
2020-04-15 11:05:55 -0400

    Use async/await instead of a promise.

--- src/workers/algorithm.js
+++ src/workers/algorithm.js
@@ -1,17 +1,18 @@
 /* TODO: fix this: Eslint must be disabled for this file because of an issue between worker-plugin and eslint-plugin. The issue causes the linter to be run twice, which breaks the build. At this point the fix is elusive... */
 /* eslint-disable */
 
+import 'regenerator-runtime'
 import { run } from '../algorithms/run'
 
-addEventListener('message', (event) => {
+addEventListener('message', async (event) => {
   const { paramsFlat, severity, ageDistribution, containment } = event.data
 
-  run(paramsFlat, severity, ageDistribution, containment).then((result) => {
-    // Delete function properties, because they cannot be cloned by the structured clone algorithm:
-    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
-    // TODO: Revisit this hack.
-    delete result.params.rate.infection
+  const result = await run(paramsFlat, severity, ageDistribution, containment)
 
-    postMessage(result)
-  })
+  // Delete function properties, because they cannot be cloned by the structured clone algorithm:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
+  // TODO: Revisit this hack.
+  delete result.params.rate.infection
+
+  postMessage(result)
 })
