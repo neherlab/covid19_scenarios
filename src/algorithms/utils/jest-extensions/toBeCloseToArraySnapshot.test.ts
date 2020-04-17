0de1aaa9194d4bbce9cc308a1034a60cb1d3bacf
abrie <abrie@users.noreply.github.com>
2020-04-16 21:44:43 -0400

    A ULP of 1 is appropriate for this test.

--- src/algorithms/utils/jest-extensions/toBeCloseToArraySnapshot.test.ts
+++ src/algorithms/utils/jest-extensions/toBeCloseToArraySnapshot.test.ts
@@ -4,6 +4,6 @@ expect.extend({ toBeCloseToArraySnapshot })
 
 describe('extension-test', () => {
   it('matches snapshot array', () => {
-    expect([1.234, 6.5542, 1.231, 999.99999]).toBeCloseToArraySnapshot(10000n)
+    expect([1.234, 6.5542, 1.231, 999.99999]).toBeCloseToArraySnapshot(1n)
   })
 })
