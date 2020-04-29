200c940672a3f694e29ef686f45a68d905cc1a70
Nicholas Noll <nbnoll@eml.cc>
2020-04-29 12:33:54 -0700

    feat: updated chart tool tip and data to have error bars

--- src/components/Main/Results/ChartTooltip.tsx
+++ src/components/Main/Results/ChartTooltip.tsx
@@ -2,12 +2,27 @@ import React from 'react'
 import { TooltipProps } from 'recharts'
 
 import { colors } from './ChartCommon'
-import { ResponsiveTooltipContent, TooltipItem } from './ResponsiveTooltipContent'
+import { ResponsiveTooltipContent, TooltipItem, TooltipPayload } from './ResponsiveTooltipContent'
 
 import './ResponsiveTooltipContent.scss'
 
 export interface ChartTooltipProps extends TooltipProps {
   valueFormatter: (value: number | string) => string
+}
+
+function dispatch(key: string, item: TooltipPayload): [number | undefined, number | undefined] {
+  switch (key) {
+    case 'peakOverflow':
+      return item.payload.errorPeakOverflow
+    case 'peakCritical':
+      return item.payload.errorPeakCritical
+    case 'peakSevere':
+      return item.payload.errorPeakSevere
+    case 'totalFatalities':
+      return item.payload.errorFatalities
+    default:
+      return [undefined, undefined]
+  }
 }
 
 export function ChartTooltip({ active, payload, label, valueFormatter, labelFormatter }: ChartTooltipProps) {
@@ -20,6 +35,8 @@ export function ChartTooltip({ active, payload, label, valueFormatter, labelForm
   const tooltipItems = payload.map(
     (payloadItem): TooltipItem => {
       const value = payloadItem.value !== undefined ? (payloadItem.value as string | number) : 0
+      const [lower, upper] =
+        typeof payloadItem.dataKey === 'string' ? dispatch(payloadItem.dataKey, payloadItem) : [undefined, undefined]
 
       return {
         name: payloadItem.name,
@@ -28,6 +45,8 @@ export function ChartTooltip({ active, payload, label, valueFormatter, labelForm
           ((payloadItem.dataKey as string) in colors ? colors[payloadItem.dataKey as string] : '#bbbbbb'),
         key: (payloadItem.dataKey as string) || payloadItem.name,
         value: valueFormatter ? valueFormatter(value) : value,
+        lower: valueFormatter ? valueFormatter(lower) : lower,
+        upper: valueFormatter ? valueFormatter(upper) : upper,
       }
     },
   )
