a299a3a37cb7cd7c879c69461148358db41b0d90
abrie <abrie@users.noreply.github.com>
2020-05-11 11:30:47 -0400

    Test verifyTuple behavior.

--- src/components/Main/Results/Utils.test.ts
+++ src/components/Main/Results/Utils.test.ts
@@ -1,12 +1,8 @@
-import { verifyPositive } from './Utils'
+import { verifyPositive, verifyTuple } from './Utils'
 
 describe('verifyPositive', () => {
   it('integer greater than zero returns same integer', () => {
     expect(verifyPositive(312)).toStrictEqual(312)
-  })
-
-  it('number greater than zero returns ceiled version', () => {
-    expect(verifyPositive(312.2)).toStrictEqual(313)
   })
 
   it('number greater than zero returns ceiled version', () => {
@@ -19,5 +15,23 @@ describe('verifyPositive', () => {
 
   it('less than zero returns undefined', () => {
     expect(verifyPositive(-1)).toBeUndefined()
+  })
+})
+
+describe('verifyTuple', () => {
+  it('input [undefined, undefined] returns undefined', () => {
+    expect(verifyTuple([undefined, undefined])).toBeUndefined()
+  })
+
+  it('input [a, undefined] returns undefined', () => {
+    expect(verifyTuple([1, undefined])).toBeUndefined()
+  })
+
+  it('input [undefined, b] returns [0.0001, b]', () => {
+    expect(verifyTuple([undefined, 2])).toStrictEqual([0.0001, 2])
+  })
+
+  it('input [a, b] returns [a,b]', () => {
+    expect(verifyTuple([1, 2])).toStrictEqual([1, 2])
   })
 })
