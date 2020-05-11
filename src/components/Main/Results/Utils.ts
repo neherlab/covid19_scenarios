28317e648b8d7a3875bfd0dca5705a834610cb9c
abrie <abrie@users.noreply.github.com>
2020-05-11 11:38:36 -0400

    Refactor: make verifyTuple types concrete.

--- src/components/Main/Results/Utils.ts
+++ src/components/Main/Results/Utils.ts
@@ -4,11 +4,9 @@ export function verifyPositive(x: number): maybeNumber {
   return x > 0 ? Math.ceil(x) : undefined
 }
 
-export function verifyTuple(
-  x: [number | undefined, number | undefined],
-): [number | undefined, number | undefined] | undefined {
+export function verifyTuple(x: [maybeNumber, maybeNumber]): [number, number] | undefined {
   if (x[0] !== undefined && x[1] !== undefined) {
-    return x
+    return [x[0], x[1]]
   }
   if (x[0] === undefined && x[1] !== undefined) {
     return [0.0001, x[1]]
