bbd095ad9f36d794f348829cfc1c5afb86ca926a
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-05-19 11:35:24 +0200

    fix: fix type issues

--- src/components/Form/RangeSpinBox.tsx
+++ src/components/Form/RangeSpinBox.tsx
@@ -16,9 +16,9 @@ export interface RangeSpinBoxProps<T> {
 export function RangeSpinBox<T>({ identifier, step, min, max, pattern, hasError }: RangeSpinBoxProps<T>) {
   return (
     <InputGroup>
-      <SpinBox identifier={`${identifier}.begin`} type="number" step={step} min={min} max={max} hasError={hasError} />
+      <SpinBox identifier={`${identifier}.begin`} type="number" step={step} min={min} max={max} />
       <span className="h-100 pt-2 px-1 text-bold">{' - '}</span>
-      <SpinBox identifier={`${identifier}.end`} type="number" step={step} min={min} max={max} hasError={hasError} />
+      <SpinBox identifier={`${identifier}.end`} type="number" step={step} min={min} max={max} />
     </InputGroup>
   )
 }
