c3895459db939e7697c45d498b507caa5c75a822
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-05-21 02:33:02 +0200

    fix: outcomes table layout

--- src/components/Main/PrintPage/TableResult.tsx
+++ src/components/Main/PrintPage/TableResult.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 import { isNil } from 'lodash'
 
 import moment from 'moment'
-import chroma from 'chroma-js'
+import classNames from 'classnames'
 import { useTranslation } from 'react-i18next'
 import { Table } from 'reactstrap'
 
 import type { AlgorithmResult } from '../../../algorithms/types/Result.types'
 import { numberFormatter } from '../../../helpers/numberFormat'
-import { colors } from '../Results/ChartCommon'
 
 import './TableResult.scss'
 
@@ -44,6 +43,7 @@ export function NumberWithUncertainty({ value, lower, upper }: NumberWithUncerta
   const valueFinite = !isNil(value) && Number.isFinite(value)
   const upperFinite = !isNil(upper) && Number.isFinite(upper)
 
+  // Three-way range
   if (valueFinite && lowerFinite && upperFinite) {
     return (
       <div>
@@ -52,13 +52,19 @@ export function NumberWithUncertainty({ value, lower, upper }: NumberWithUncerta
     )
   }
 
+  // Just value
   if ((!lowerFinite || !upperFinite) && valueFinite) {
     return <div>{numberFormat(value)}</div>
   }
 
+  // Two-way range ("from - $to")
   if (!valueFinite && lowerFinite && upperFinite) {
-    if (lower === upper) {
-      return <div>{numberFormat(lower)}</div>
+    const lowerFormatted = numberFormat(lower)
+    const upperFormatted = numberFormat(upper)
+
+    // Avoid formats like "0 - 0" and "5k - 5k"
+    if (lowerFormatted === upperFormatted) {
+      return <div>{lowerFormatted}</div>
     }
 
     return (
@@ -68,19 +74,17 @@ export function NumberWithUncertainty({ value, lower, upper }: NumberWithUncerta
     )
   }
 
+  // Catch-all value is zero
   return <div>{'0'}</div>
-}
-
-export function pale(color: string): string {
-  return chroma(color).alpha(0.1).hex()
 }
 
 export interface TableResultProps {
   result?: AlgorithmResult
+  forPrint?: boolean
   formatter?: (num: number) => string
 }
 
-export default function TableResult({ result, formatter = Number.toString }: TableResultProps) {
+export default function TableResult({ result, forPrint, formatter = Number.toString }: TableResultProps) {
   const { t } = useTranslation()
 
   if (!result) {
@@ -94,67 +98,69 @@ export default function TableResult({ result, formatter = Number.toString }: Tab
   }
 
   return (
-    <Table className="outcomes-table">
-      <thead>
-        <tr>
-          <th scope="col" className="text-center col-auto" style={{ width: '3.5cm' }}>
-            <b>{t(`Date`)}</b>
-          </th>
-          <th scope="col" className="text-center col-auto" style={{ backgroundColor: pale(colors.severe) }}>
-            <b>{t(`Severe`)}</b>
-          </th>
-          <th scope="col" className="text-center col-auto" style={{ backgroundColor: pale(colors.critical) }}>
-            <b>{t(`Critical`)}</b>
-          </th>
-          <th scope="col" className="text-center col-auto" style={{ backgroundColor: pale(colors.overflow) }}>
-            <b>{t(`ICU Overflow`)}</b>
-          </th>
-          <th scope="col" className="text-center col-auto" style={{ backgroundColor: pale(colors.fatality) }}>
-            <b>{t(`Deaths`)}</b>
-          </th>
-          <th scope="col" className="text-center col-auto" style={{ backgroundColor: pale(colors.recovered) }}>
-            <b>{t(`Recovered`)}</b>
-          </th>
-        </tr>
-      </thead>
-
-      <tbody className="outcomes-table-body">
-        {downsampled.middle.map((line, i) => (
-          <tr key={line.time}>
-            <th scope="row">{dateFormat(line.time)}</th>
-            <td style={{ backgroundColor: pale(colors.severe) }}>
-              <NumberWithUncertainty
-                lower={downsampled.lower[i].current.severe.total}
-                upper={downsampled.upper[i].current.severe.total}
-              />
-            </td>
-            <td style={{ backgroundColor: pale(colors.critical) }}>
-              <NumberWithUncertainty
-                lower={downsampled.lower[i].current.critical.total}
-                upper={downsampled.upper[i].current.critical.total}
-              />
-            </td>
-            <td style={{ backgroundColor: pale(colors.overflow) }}>
-              <NumberWithUncertainty
-                lower={downsampled.lower[i].current.overflow.total}
-                upper={downsampled.upper[i].current.overflow.total}
-              />
-            </td>
-            <td style={{ backgroundColor: pale(colors.fatality) }}>
-              <NumberWithUncertainty
-                lower={downsampled.lower[i].cumulative.fatality.total}
-                upper={downsampled.upper[i].cumulative.fatality.total}
-              />
-            </td>
-            <td style={{ backgroundColor: pale(colors.recovered) }}>
-              <NumberWithUncertainty
-                lower={downsampled.lower[i].cumulative.recovered.total}
-                upper={downsampled.upper[i].cumulative.recovered.total}
-              />
-            </td>
+    <div className={classNames('outcomes-table-wrapper', forPrint && 'print-preview')}>
+      <Table className={classNames('outcomes-table', forPrint && 'print-preview')} bordered>
+        <thead>
+          <tr>
+            <th scope="col" className="outcome-table-th outcome-table-th-date">
+              {t(`Date`)}
+            </th>
+            <th scope="col" className="outcome-table-th bg-severe">
+              {t(`Severe`)}
+            </th>
+            <th scope="col" className="outcome-table-th bg-critical">
+              {t(`Critical`)}
+            </th>
+            <th scope="col" className="outcome-table-th bg-overflow">
+              {t(`ICU Overflow`)}
+            </th>
+            <th scope="col" className="outcome-table-th bg-fatality">
+              {t(`Deaths`)}
+            </th>
+            <th scope="col" className="outcome-table-th bg-recovered">
+              {t(`Recovered`)}
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+
+        <tbody className="outcomes-table-body">
+          {downsampled.middle.map((line, i) => (
+            <tr key={line.time}>
+              <th scope="row">{dateFormat(line.time)}</th>
+              <td className="bg-severe">
+                <NumberWithUncertainty
+                  lower={downsampled.lower[i].current.severe.total}
+                  upper={downsampled.upper[i].current.severe.total}
+                />
+              </td>
+              <td className="bg-critical">
+                <NumberWithUncertainty
+                  lower={downsampled.lower[i].current.critical.total}
+                  upper={downsampled.upper[i].current.critical.total}
+                />
+              </td>
+              <td className="bg-overflow">
+                <NumberWithUncertainty
+                  lower={downsampled.lower[i].current.overflow.total}
+                  upper={downsampled.upper[i].current.overflow.total}
+                />
+              </td>
+              <td className="bg-fatality">
+                <NumberWithUncertainty
+                  lower={downsampled.lower[i].cumulative.fatality.total}
+                  upper={downsampled.upper[i].cumulative.fatality.total}
+                />
+              </td>
+              <td className="bg-recovered">
+                <NumberWithUncertainty
+                  lower={downsampled.lower[i].cumulative.recovered.total}
+                  upper={downsampled.upper[i].cumulative.recovered.total}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   )
 }
