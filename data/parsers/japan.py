e686e715c639af77bf8b058ce0f05bf9c3b1ba21
Richard Neher <richard.neher@unibas.ch>
2020-05-22 15:28:59 +0200

    fix names of japan case count tsv

--- data/parsers/japan.py
+++ data/parsers/japan.py
@@ -10,15 +10,15 @@ cols = ['time', 'cases', 'deaths', 'hospitalized', 'icu', 'recovered']
 # Main point of entry
 
 def parse():
-    
+
     #Data extraction from source
     dataframe=pd.read_csv("https://raw.githubusercontent.com/kaz-ogiwara/covid19/master/data/prefectures.csv")
-    
+
     #Time format conversion
     time_format = '%Y-%m-%d'
-    dataframe['date'] = pd.to_datetime(dataframe.year.astype(str)+'-'+dataframe.month.astype(str)+'-'+dataframe.date.astype(str), format=time_format) 
-    
-    #Separate by region 
+    dataframe['date'] = pd.to_datetime(dataframe.year.astype(str)+'-'+dataframe.month.astype(str)+'-'+dataframe.date.astype(str), format=time_format)
+
+    #Separate by region
     dummy=pd.get_dummies(dataframe['prefectureNameE'])
     dataframe=pd.concat([dataframe,dummy],axis=1)
     regions_name=dataframe.iloc[:,9:].columns
@@ -33,15 +33,15 @@ def parse():
 
     region_tables = {}
     for region in regions_name:
-        region_tables['-'.join(['JP',region])]  = dataframe_region[dataframe[region]==1].values.tolist()
-    
+        region_tables['-'.join(['JPN',region])]  = dataframe_region[dataframe[region]==1].values.tolist()
+
     #All Japan cases
     #Data extraction from source
     dataframe=pd.read_csv("https://raw.githubusercontent.com/kaz-ogiwara/covid19/master/data/summary.csv")
-    
+
     #Time format conversion
     time_format = '%Y-%m-%d'
-    dataframe['date'] = pd.to_datetime(dataframe.year.astype(str)+'-'+dataframe.month.astype(str)+'-'+dataframe.date.astype(str), format=time_format) 
+    dataframe['date'] = pd.to_datetime(dataframe.year.astype(str)+'-'+dataframe.month.astype(str)+'-'+dataframe.date.astype(str), format=time_format)
     dataframe['date']=dataframe['date'].astype(str)
 
     dataframe_japan=dataframe[['date', 'pcr_tested_positive', 'death', 'hospitalized', 'serious', 'discharged']].copy()
