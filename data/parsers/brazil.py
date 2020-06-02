5902db410facaed72da6a4dcb3ae647eb65a6d55
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-02 10:40:48 +0200

    fix: subtract imported cases in case counts parser for Brazil
    The code is by @pauloangelo from 
    https://github.com/neherlab/covid19_scenarios/issues/718#issuecomment-636916753

--- data/parsers/brazil.py
+++ data/parsers/brazil.py
@@ -57,6 +57,23 @@ def parse():
     rdr = csv.reader(fd)
     hdr = next(rdr)
 
+    # Added block
+    regions_external = {}
+    for row in rdr:
+        state = '-'.join(['BRA',state_codes[row[1]]])
+        city = row[2]
+        if city != "Importados/Indefinidos": continue
+        date = row[0]
+        cases = stoi(row[4])
+        deaths = stoi(row[5])
+        if state not in regions_external:
+            regions_external[state] = {}
+        regions_external[state][date] = [cases, deaths, None, None, None]
+
+    fd  = io.StringIO(r.text)
+    rdr = csv.reader(fd)
+    hdr = next(rdr)
+
     for row in rdr:
         state = '-'.join(['BRA',state_codes[row[1]]])
         city = row[2]
@@ -64,7 +81,13 @@ def parse():
         date = row[0]
         cases = stoi(row[4])
         deaths = stoi(row[5])
-        regions[state].append([date, cases, deaths, None, None, None])
+
+        # remove the imported/undefined counts
+        if state in regions_external and date in regions_external[state]:
+            regions[state].append([date, cases-regions_external[state][date][0], deaths-regions_external[state][date][1], None, None, None])
+        else:
+            regions[state].append([date, cases, deaths, None, None, None])
+
     for state, data in regions.items():
         regions[state] = sorted_date(data, cols)
 
