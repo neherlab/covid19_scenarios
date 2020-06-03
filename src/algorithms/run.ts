448fd42e31444215010a429a6375515ac86dfc4c
Richard Neher <richard.neher@unibas.ch>
2020-06-03 11:08:42 +0200

    refactor handling of stochastic/deterministic simulations

--- src/algorithms/run.ts
+++ src/algorithms/run.ts
@@ -1,7 +1,8 @@
+import { AgeGroup } from '../.generated/latest/types'
 import { AgeDistributionDatum, ScenarioFlat, SeverityDistributionDatum } from './types/Param.types'
-import { AlgorithmResult, SimulationTimePoint, ExportedTimePoint } from './types/Result.types'
+import { AlgorithmResult, SimulationTimePoint, ExportedTimePoint, ModelParams } from './types/Result.types'
 
-import { getPopulationParams, initializePopulation } from './initialize'
+import { getPopulationParams, initializePopulation, withUncertainty } from './initialize'
 import { collectTotals, evolve } from './model'
 import { percentileTrajectory } from './results'
 import { preparePlotData } from './preparePlotData'
@@ -14,52 +15,62 @@ export interface RunParams {
   ageDistribution: AgeDistributionDatum[]
 }
 
+function simulate(
+  initialState: SimulationTimePoint,
+  params: ModelParams,
+  tMax: number,
+  ageGroups: AgeGroup[],
+  func: (x: number) => number,
+): ExportedTimePoint[] {
+  const dynamics = [initialState]
+  let currState = initialState
+
+  while (currState.time < tMax) {
+    currState = evolve(currState, params, currState.time + 1, func)
+    dynamics.push(currState)
+  }
+
+  return collectTotals(dynamics, ageGroups)
+}
+
 export async function run({ params, severity, ageDistribution }: RunParams): Promise<AlgorithmResult> {
   const tMin: number = new Date(params.simulationTimeRange.begin).getTime()
   const tMax: number = new Date(params.simulationTimeRange.end).getTime()
   const ageGroups = ageDistribution.map((d) => d.ageGroup)
   const initialCases = params.initialNumberOfCases
 
-  const modelParamsArray = getPopulationParams(params, severity, ageDistribution, false)
-  modelParamsArray.push(...getPopulationParams(params, severity, ageDistribution, true))
+  const stochastic = withUncertainty(params)
 
-  const trajectories = modelParamsArray.map((modelParams) => {
-    const population = initializePopulation(modelParams.populationServed, initialCases, tMin, ageDistribution)
-    function simulate(initialState: SimulationTimePoint, func: (x: number) => number): ExportedTimePoint[] {
-      const dynamics = [initialState]
-      let currState = initialState
-
-      while (currState.time < tMax) {
-        currState = evolve(currState, modelParams, currState.time + 1, func)
-        dynamics.push(currState)
-      }
-
-      return collectTotals(dynamics, ageGroups)
-    }
-    return simulate(population, identity)
-  })
-
-  // const mean = meanTrajectory(trajectories)
-  // const sdev = stddevTrajectory(trajectories, mean)
+  let stochasticTrajectories: ExportedTimePoint[][] = []
+  let stochasticParams: ModelParams[] = []
+  if (stochastic) {
+    stochasticParams = getPopulationParams(params, severity, ageDistribution, false)
+    stochasticTrajectories = stochasticParams.map((modelParams) => {
+      const tmpPopulation = initializePopulation(modelParams.populationServed, initialCases, tMin, ageDistribution)
+      return simulate(tmpPopulation, modelParams, tMax, ageGroups, identity)
+    })
+  }
+  const deterministicParams = getPopulationParams(params, severity, ageDistribution, true)[0]
+  const population = initializePopulation(deterministicParams.populationServed, initialCases, tMin, ageDistribution)
+  const deterministicTrajectory = simulate(population, deterministicParams, tMax, ageGroups, identity)
 
   const thresholds = [0.2, 0.5, 0.8]
-  const idxs = thresholds.map((d) => Math.ceil((trajectories.length - 1) * d))
-  const R0Trajectories = trajectories[0].map((d) => {
-    return {
-      t: d.time,
-      y: modelParamsArray
-        .slice(0, -1)
-        .map((ModelParams) => ModelParams.rate.infection(d.time) * params.infectiousPeriodDays)
-        .sort((a, b) => a - b),
-    }
-  })
+  const idxs = thresholds.map((d) => Math.ceil((stochasticTrajectories.length - 1) * d))
+  const R0Trajectories = stochastic
+    ? deterministicTrajectory.map((d) => {
+        return {
+          t: d.time,
+          y: stochasticParams
+            .map((ModelParams) => ModelParams.rate.infection(d.time) * params.infectiousPeriodDays)
+            .sort((a, b) => a - b),
+        }
+      })
+    : []
 
-  const meanTrajectory = trajectories[trajectories.length - 1]
-  const stochasticTrajectories = trajectories.slice(0, -1)
-  const meanR0Trajectory = meanTrajectory.map((d) => {
+  const meanR0Trajectory = deterministicTrajectory.map((d) => {
     return {
       t: d.time,
-      y: modelParamsArray[modelParamsArray.length - 1].rate.infection(d.time) * params.infectiousPeriodDays,
+      y: deterministicParams.rate.infection(d.time) * params.infectiousPeriodDays,
     }
   })
   const resultsTrajectory = {
@@ -72,8 +83,8 @@ export async function run({ params, severity, ageDistribution }: RunParams): Pro
     trajectory: resultsTrajectory,
     R0: {
       mean: meanR0Trajectory,
-      lower: R0Trajectories.map((d) => ({ t: d.t, y: d.y[idxs[0]] })),
-      upper: R0Trajectories.map((d) => ({ t: d.t, y: d.y[idxs[2]] })),
+      lower: stochastic ? R0Trajectories.map((d) => ({ t: d.t, y: d.y[idxs[0]] })) : meanR0Trajectory,
+      upper: stochastic ? R0Trajectories.map((d) => ({ t: d.t, y: d.y[idxs[2]] })) : meanR0Trajectory,
     },
     plotData: preparePlotData(resultsTrajectory),
   }
