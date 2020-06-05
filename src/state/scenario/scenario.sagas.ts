533fd806f2bcc97d8d904336bbc017e850b7b4d1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-05 17:17:26 +0200

    fix: make sure algroithm reruns when data is changed

--- src/state/scenario/scenario.sagas.ts
+++ src/state/scenario/scenario.sagas.ts
@@ -1,8 +1,9 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { put, takeEvery, takeLatest } from 'redux-saga/effects'
 
 import { LOCATION_CHANGE, LocationChangeAction } from 'connected-react-router'
 import { fromUrl } from '../../io/serialization/fromUrl'
-import { setScenarioState } from './scenario.actions'
+import { algorithmRunTrigger } from '../algorithm/algorithm.actions'
+import { setScenario, setScenarioData, setScenarioState } from './scenario.actions'
 
 export function* processUrl({ payload: { location } }: LocationChangeAction) {
   const { pathname, search } = location
@@ -13,4 +14,13 @@ export function* processUrl({ payload: { location } }: LocationChangeAction) {
   }
 }
 
-export default [takeEvery(LOCATION_CHANGE, processUrl)]
+export function* triggerAlgorithm() {
+  yield put(algorithmRunTrigger())
+}
+
+export default [
+  takeEvery(LOCATION_CHANGE, processUrl),
+  takeLatest(setScenario, triggerAlgorithm),
+  takeLatest(setScenarioData, triggerAlgorithm),
+  takeLatest(setScenarioState, triggerAlgorithm),
+]
