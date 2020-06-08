be0c6e68708d355bec57b9dce0c545dfe793cfba
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-09 01:29:57 +0200

    fix: resolve error due to lodash being optimized too much

--- config/next/withLodash.ts
+++ config/next/withLodash.ts
@@ -4,7 +4,28 @@ import LodashWebpackPlugin, { Options } from 'lodash-webpack-plugin'
 import { addWebpackPlugin } from './lib/addWebpackPlugin'
 
 const withLodash = (options: Options) => (nextConfig: NextConfig) => {
-  return addWebpackPlugin(nextConfig, new LodashWebpackPlugin(options))
+  return addWebpackPlugin(
+    nextConfig,
+    new LodashWebpackPlugin({
+      caching: true,
+      chaining: true,
+      cloning: true,
+      coercions: true,
+      collections: true,
+      currying: true,
+      deburring: true,
+      exotics: true,
+      flattening: true,
+      guards: true,
+      memoizing: true,
+      metadata: true,
+      paths: true,
+      placeholders: true,
+      shorthands: true,
+      unicode: true,
+      ...options,
+    }),
+  )
 }
 
 export default withLodash
