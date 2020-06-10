d94ae0667ca06534ce5a95d370be2716e004dcbc
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-11 00:58:38 +0200

    refactor: reduce duplication, improve readability

--- src/algorithms/preparePlotData.ts
+++ src/algorithms/preparePlotData.ts
@@ -1,6 +1,16 @@
+import { pickBy, mapValues } from 'lodash'
+
 import type { Trajectory, PlotDatum } from './types/Result.types'
 import { verifyPositive, verifyTuple } from '../components/Main/Results/Utils'
 // import { linesToPlot, areasToPlot, DATA_POINTS } from '../components/Main/Results/ChartCommon'
+
+export function filterPositiveValues<T extends { [key: string]: number }>(obj: T) {
+  return pickBy(obj, (value) => value > 0) as T
+}
+
+export function roundValues<T extends { [key: string]: number }>(obj: T) {
+  return mapValues(obj, verifyPositive) as T
+}
 
 export function preparePlotData(trajectory: Trajectory): PlotDatum[] {
   const { lower, middle, upper } = trajectory
@@ -8,25 +18,30 @@ export function preparePlotData(trajectory: Trajectory): PlotDatum[] {
   return middle.map((x, day) => {
     const previousDay = day > 6 ? day - 7 : 0
     const centerWeeklyDeaths = x.cumulative.fatality.total - middle[previousDay].cumulative.fatality.total
+
     // NOTE: this is using the upper and lower trajectories
     const extremeWeeklyDeaths1 = upper[day].cumulative.fatality.total - upper[previousDay].cumulative.fatality.total
     const extremeWeeklyDeaths2 = lower[day].cumulative.fatality.total - lower[previousDay].cumulative.fatality.total
     const upperWeeklyDeaths = extremeWeeklyDeaths1 > extremeWeeklyDeaths2 ? extremeWeeklyDeaths1 : extremeWeeklyDeaths2
     const lowerWeeklyDeaths = extremeWeeklyDeaths1 > extremeWeeklyDeaths2 ? extremeWeeklyDeaths2 : extremeWeeklyDeaths1
 
+    let lines = {
+      susceptible: x.current.susceptible.total,
+      infectious: x.current.infectious.total,
+      severe: x.current.severe.total,
+      critical: x.current.critical.total,
+      overflow: x.current.overflow.total,
+      recovered: x.cumulative.recovered.total,
+      fatality: x.cumulative.fatality.total,
+      weeklyFatality: centerWeeklyDeaths,
+    }
+
+    lines = filterPositiveValues(lines)
+    lines = roundValues(lines)
+
     return {
       time: x.time,
-      lines: {
-        susceptible: verifyPositive(x.current.susceptible.total),
-        infectious: verifyPositive(x.current.infectious.total),
-        severe: verifyPositive(x.current.severe.total),
-        critical: verifyPositive(x.current.critical.total),
-        overflow: verifyPositive(x.current.overflow.total),
-        recovered: verifyPositive(x.cumulative.recovered.total),
-        fatality: verifyPositive(x.cumulative.fatality.total),
-        weeklyFatality: verifyPositive(centerWeeklyDeaths),
-      },
-      // Error bars
+      lines,
       areas: {
         susceptible: verifyTuple(
           [verifyPositive(lower[day].current.susceptible.total), verifyPositive(upper[day].current.susceptible.total)],
