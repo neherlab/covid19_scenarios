4e49a0d8c8c8d5e13be241e16a6732af2bcb216c
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-12 07:56:51 +0200

    fix: adjust path in tsc ignore list

--- config/webpack/webpack.client.babel.ts
+++ config/webpack/webpack.client.babel.ts
@@ -291,7 +291,7 @@ export default {
           '!src/algorithms/model.ts', // FIXME
           '!src/algorithms/results.ts', // FIXME
           '!src/components/Main/Results/AgeBarChart.tsx', // FIXME
-          '!src/components/Main/Results/ResultsTrajectoriesPlot.tsx', // FIXME
+          '!src/components/Main/ResultsTrajectoriesPlot/ResultsTrajectoriesPlot.tsx', // FIXME
           // end
 
           '!src/**/*.(spec|test).{js,jsx,ts,tsx}',
