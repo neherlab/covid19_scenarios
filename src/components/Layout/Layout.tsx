a1a17ad3368c47870dc3c2ba07c12027f98d0477
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-29 06:23:35 +0200

    fix: make container fluid again

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ import Footer from './Footer'
 
 export function Layout({ children }: PropsWithChildren<HTMLProps<HTMLDivElement>>) {
   return (
-    <Container className="layout-container">
+    <Container fluid className="layout-container px-0">
       <header className="row navbar-container d-print-none">
         <NavigationBar />
       </header>
