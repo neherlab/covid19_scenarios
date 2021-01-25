fa8bd615cfb48d6160b6d3462ddfed5a832d97ad
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-01-25 14:37:55 +0100

    chore(ci): install awscli manually
    pip install takes more than a minute, which is 20% of build time

---
+++ tools/install-awscli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Creates CircleCI contexts and sets environment variables
+# Requires CircleCI Local CLI: https://circleci.com/docs/2.0/local-cli/
+
+set -o errexit
+set -o pipefail
+shopt -s dotglob
+trap "exit" INT
+
+THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+
+pushd /tmp >/dev/null
+
+curl -fsS "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -oqq awscliv2.zip
+./aws/install --update
+
+popd >/dev/null
